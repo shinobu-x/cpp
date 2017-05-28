6faf4468c326ef1df756d1e9b4601620e0105c72
Shinobu Kinjo <shinobu@redhat.com>
2017-05-29 08:01:39 +0900

    modified:   templates/operator/copy_op_v1.cpp

--- templates/operator/copy_op_v1.cpp
+++ templates/operator/copy_op_v1.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 template <typename T>
-struct TEST_T {
-  const int value = 1;
+struct BASE_T {};
 
-  TEST_T& operator=(const TEST_T& that) {
-    static_cast<T&>(*this) = that;
+template <typename T>
+struct TEST_T : public BASE_T<T> {
+  int value;
+
+  TEST_T& operator=(const BASE_T<T>& that) {
+    static_cast<BASE_T<T>& >(*this) = that;
     return *this;
   }
 };
@@ -13,7 +16,11 @@ struct TEST_T {
 template <typename T>
 T doit() {
  TEST_T<T> a;
- return a.value;
+ TEST_T<T> b;
+ a.value = 1;
+ b.value = 2;
+ b.value = a.value;
+ return b.value;
 }
 
 auto main() -> int
