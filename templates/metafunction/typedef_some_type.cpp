e5581d1b71c08d53c266dff08abd661f67ad6b1f
Shinobu Kinjo <shinobu@redhat.com>
2017-06-01 08:33:17 +0900

    new file:   templates/metafunction/typedef_some_type.cpp

---
+++ templates/metafunction/typedef_some_type.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+template <typename T>
+struct largest_precision_type; /// Nothing!
+
+template < >
+struct largest_precision_type<float> {
+  typedef float type;
+};
+
+template < >
+struct largest_precision_type<double> {
+  typedef double type;
+};
+
+template < >
+struct largest_precision_type<int> {
+  typedef long type;
+};
+
+template <unsigned int N>
+struct powerof_2 {
+  static const unsigned int value = (1<<N);
+};
+
+template <unsigned int N>
+struct more_powerof_2 {
+  enum { value = (1<<N) };
+};
+
+template <typename T>
+T doit() {
+  auto a = powerof_2<5>::value;
+  auto b = more_powerof_2<10>::value;
+  std::cout << a << '\n';
+  std::cout << b << '\n';
+}
+
+auto main() -> int
+{
+  doit<void>();
+  return 0;
+}
