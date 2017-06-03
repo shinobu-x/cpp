5d4997e4f0b25885c5e44b408e98e6904aa80b23
Shinobu Kinjo <shinobu@redhat.com>
2017-06-03 10:50:36 +0900

    new file:   test/allocator/allocator_v1.cpp

---
+++ test/allocator/allocator_v1.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <iostream>
+
+template <typename T>
+T xalloc(T** a, T s, T v) {
+  *a = (T*)(malloc(sizeof(T)*s));
+  if (*a != NULL)
+    for (int i = 0; i < s; ++i)
+      *(*a + i) = v;
+}
+
+template <typename T>
+T doit() {
+  T* v = NULL;
+  T x = 5;
+  T y = 1;
+  xalloc(&v, x, y);
+
+//  for (T i = 0; i < x; ++i)
+//    std::cout << v[i] << '\n';
+
+  v = NULL;
+  free(v);
+}
+
+auto main() -> int
+{
+  typedef int type;
+  type* v = NULL;
+  doit<type>();
+} 
