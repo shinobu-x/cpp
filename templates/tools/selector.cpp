79ef55f96d4677d6906dfbecd0025f685fe0b3bc
Shinobu Kinjo <shinobu@redhat.com>
2017-06-04 12:57:38 +0900

    new file:   templates/tools/selector.cpp

---
+++ templates/tools/selector.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+template <bool condition>
+struct selector { };
+
+typedef selector<true> true_type;
+typedef selector<false> false_type;
+
+template <bool condition, typename T>
+void f(const T& x) {
+  if (condition) std::cout << x << '\n';
+  else std::cout << "False" << '\n';
+}
+
+template <bool condition, typename T>
+void f(selector<condition>, const T& x) {
+  if (condition) std::cout << x << '\n';
+  else std::cout << "False" << '\n';
+}
+
+template <typename T>
+T doit() {
+  T a = 1;
+  f<true>(a);
+  f<false>(a);
+  f(selector<true>(), a);
+  f(selector<false>(), a);
+  f(true_type(), a);
+  f(false_type(), a);
+}
+
+auto main() -> int
+{
+  doit<int>();
+  return 0;
+}
