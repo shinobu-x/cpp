3ce38829f7b511b0f00ee167072a1b65a030199d
Shinobu Kinjo <shinobu@redhat.com>
2017-06-10 18:53:07 +0900

    modified:   templates/swap_and_sort/swap_and_sort.cpp

--- templates/swap_and_sort/swap_and_sort.cpp
+++ templates/swap_and_sort/swap_and_sort.cpp
@@ -6,13 +6,20 @@
 template <typename T>
 class sorted_vector {
 public:
-  void swap(srted_vector<T>& that) {
+  void swap(sorted_vector<T>& that) {
     data_.swap(that.data_);
   }
 
   void swap(std::vector<T>& that) {
     data_.swap(that);
     std::sort(data_.begin(), data_.end());
+  }
+
+  void load_data() {
+    std::random_device rd;
+    for (int i = 0; i < 10; ++i) {
+      data_.push_back(rd());
+    }
   }
 
 private:
@@ -26,11 +33,17 @@ T doit() {
 
   std::random_device rd;
 
-  for (int i = 0; i < 10; ++) {
-    std::cout << rd() << '\n';
+  for (int i = 0; i < 10; ++i) {
+    y.push_back(rd());
   }
 
+  x.load_data();
+  x.swap(y);
+
+}
+typedef unsigned int type;
 auto main() -> int
 {
+  doit<type>();
   return 0;
 }
