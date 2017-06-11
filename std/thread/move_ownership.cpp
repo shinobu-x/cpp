851a63a9c64734e18739826c6a5abf99de6e8da7
Shinobu Kinjo <shinobu@redhat.com>
2017-06-11 13:52:05 +0900

    modified:   std/thread/move_ownership.cpp

--- std/thread/move_ownership.cpp
+++ std/thread/move_ownership.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
-#include <system_error>
 #include <thread>
-#include <utility>
+#include <vector>
+
+void f1(std::thread t) {
+  t.join();
+  std::cout << __func__ << ": " << std::this_thread::get_id() << '\n';
+}
+
+template <typename T>
+T f2() {
+  std::vector<int> v;
+  std::thread t([&v](int x) {
+      for (T i = 0; i < x; ++i)
+        v.push_back(i);
+    }, 10
+  );
+
+  f1(std::move(t));
+}
 
 template <typename T>
 T doit() {
-  std::thread t1(
-    []{ std::cout << std::this_thread::get_id() << '\n'; }
-  );
-  std::thread t2;
-
-  t2 = std::move(t1);
-  t2.join();
+  f2<T>();
 }
 
 auto main() -> int
