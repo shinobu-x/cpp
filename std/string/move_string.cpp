125a54648515436eeb8e0a59b790ec75875996b9
Shinobu Kinjo <shinobu@redhat.com>
2017-06-11 13:20:17 +0900

    modified:   std/string/move_string.cpp

--- std/string/move_string.cpp
+++ std/string/move_string.cpp
@@ -5,11 +5,14 @@ enum optimization {
   LEVEL1,
   LEVEL2,
   LEVEL3,
-  LEVEL4
+  LEVEL4,
+  LEVEL5
 };
 
 template <typename T, T LEVEL>
-T doit(std::string& s) {
+T doit(std::string& s, std::string& r0) {
+  r0.clear();
+  r0.reserve(s.length());
   std::string r1;
   std::string r2;  // Reseve storage space
   r2.reserve(s.length());
@@ -18,24 +21,28 @@ T doit(std::string& s) {
       switch (LEVEL) {
       case LEVEL1:
         r1 = r1 + s[i];  // With temporary storage
-        break;
       case LEVEL2:
         r1 += s[i];  // Without temporary storage
-        break;
       case LEVEL3:
         r2 += s[i];
-        break;
       case LEVEL4:
-        goto fast;
+        goto faster;
         break;
       }
     }
   }
-fast:
+faster:
 {
-  for (auto it = s.begin(), end = s.end(); it != end; ++it)
-    if (*it >= 0x20)
-      r2 += *it;
+  for (auto it = s.begin(), end = s.end(); it != end; ++it) {
+    if (*it >= 0x20) {
+      switch (LEVEL) {
+      case LEVEL4:
+        r2 += *it;
+      case LEVEL5:
+        r0 += *it;
+      }
+    }
+  }
 }
   return 0;
 }
@@ -43,5 +50,6 @@ fast:
 auto main() -> int
 {
   std::string s = {"1234abcd"};
-  doit<int, 3>(s);
+  std::string r;
+  doit<int, 4>(s, r);
 }
