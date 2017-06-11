8d7d2fdcbf8611c93253ecf44259a50dcd5d2898
Shinobu Kinjo <shinobu@redhat.com>
2017-06-11 11:57:29 +0900

    new file:   std/string/remove_char.cpp

---
+++ std/string/remove_char.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+// #include <string>
+
+template <bool condition>
+struct selector {};
+
+typedef selector<true> true_type;
+typedef selector<false> false_type;
+
+template <typename T, bool is_optimize>
+T doit(std::string& s) {
+  std::string r;
+  for (T i = 0; i < s.length(); ++i) {
+    if (s[i] >= 0x20) {
+      if (is_optimize)
+        r += s[i];
+      else if (!is_optimize)
+        r = r + s[i];
+    }
+  }
+}
+
+auto main() -> int
+{
+  std::string s = {"1234abcd"};
+  doit<int, true>(s);
+}
