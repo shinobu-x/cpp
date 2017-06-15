0d387898ac2dd7c080f751d63124e65ca0778b55
Shinobu Kinjo <shinobu@redhat.com>
2017-06-16 07:34:47 +0900

    modified:   templates/tools/digit.cpp

--- templates/tools/digit.cpp
+++ templates/tools/digit.cpp
@@ -5,7 +5,7 @@ struct digit;
 
 template <int I, int N>
 struct digit {
-  static const int value = digit<i-1, N/10>::value;
+  static const int value = digit<I-1, N/10>::value;
 };
 
 template <int N>
@@ -15,9 +15,12 @@ struct digit<0, N> {
 
 template <typename T>
 T doit() {
+  const T a = 2, b = 33893;
+  std::cout << digit<a, b>::value << '\n';
 }
 
 auto main() -> int
 {
+  doit<int>();
   return 0;
 }
