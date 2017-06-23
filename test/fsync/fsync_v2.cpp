8c0fa4d6097eed8b75f93e21d9f3289189d7f42f
Shinobu Kinjo <shinobu@redhat.com>
2017-06-23 16:51:56 +0900

    new file:   test/fsync/fsync_v2.cpp

---
+++ test/fsync/fsync_v2.cpp
@@ -0,0 +1,80 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <iostream>
+#include <thread>
+#include <vector>
+
+#define SIZE 4096
+#define JOBS 1000
+#define LOOP 10
+
+int n_write=0, n_fsync=0;
+
+template <typename T, T N>
+T do_fsync() {
+  char *b;
+  int fd, r;
+  char file[]="/var/lib/nova/instances/test.txt";
+
+  std::cout << std::this_thread::get_id() << '\n';
+
+  for (T i=0; i<LOOP; ++i) {
+    if ((b = (char*)malloc(N)) == NULL)
+      perror("Error: malloc");
+    else if ((fd = creat(file, S_IWUSR)) < 0)
+      perror("Error: creat");
+    else {
+      memset(b, 's', N);
+
+      if ((r = write(fd, b, N)) == -1)
+        perror("Error: write");
+      else {
+        n_write++;
+        std::cout << "Write: " << n_write;
+        std::cout << " / ";
+        std::cout << "Fsync: " << n_fsync;
+        std::cout  << '\n';
+        // std::cout << "Write " << r << " bytes\n";
+
+        if (fsync(fd) != 0)
+          perror("Error: fsync");
+        else if ((r = write(fd, b, N)) == -1)
+          perror("Error: write");
+        else {
+          n_fsync++;
+          // std::cout << "Fsync: " << n_fsync << '\n';
+          // std::cout << "Wrote " << r << " bytes\n";
+         }
+      }
+    }
+
+    close(fd);
+    unlink(file);
+  }
+  return 0;
+}
+
+template <typename T, T N>
+T doit() {
+  std::vector<std::thread> v;
+
+
+  for (T i=0; i<N; ++i)
+    v.push_back(std::thread(do_fsync<T, SIZE>));
+
+  for (auto& t : v)
+    t.join();
+}
+
+auto main() -> int
+{
+  doit<int, JOBS>();
+  return 0;
+}
