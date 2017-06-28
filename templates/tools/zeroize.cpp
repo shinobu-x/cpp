c5e1444db96a2a6889f530dcf06dbfefbcb98d9c
Shinobu Kinjo <shinobu@redhat.com>
2017-06-29 08:24:05 +0900

    new file:   templates/tools/zeroize.cpp

---
+++ templates/tools/zeroize.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+
+template <typename T, T N>
+struct static_parameter {};
+
+template <typename T, T N>
+struct static_value : static_parameter<T, N> {
+  const static T value = N;
+};
+
+template <typename T>
+struct zeroize_helper {
+  static void apply(T* const data, static_value<int, 1>) {
+    *data = T();
+  }
+
+  static void apply(T (&data)[2], static_value<int, 2>) {
+    data[0] = data[1] = T();
+  }
+
+  static void apply(T* const data, const int N) {
+    std::fill_n(data, N, T());
+  }
+
+  static T test(T* const data, const int N) {
+    return N;
+  }
+};
+
+template <typename T, int N>
+void zeroize(T (&data)[N]) {
+  zeroize_helper<T>::test(data, N);
+}
+
+template <typename T>
+T doit() {
+  T a[5];
+  zeroize<T, 5>(a);
+}
+
+auto main() -> int
+{
+  doit<int>();
+}
