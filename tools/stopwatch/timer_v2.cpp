4f36ea2964cf1f458e942df9266c7d0d110ac235
Shinobu Kinjo <shinobu@redhat.com>
2017-07-16 07:39:53 +0900

    new file:   tools/stopwatch/timer_v1.cpp new file:   tools/stopwatch/timer_v2.cpp

---
+++ tools/stopwatch/timer_v2.cpp
@@ -0,0 +1,36 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+class timer {
+public:
+  timer() : start_(clock_::now()) {}
+
+  void reset() {
+    reset_();
+  }
+
+  friend std::ostream& operator<< (std::ostream& out, timer const &t) {
+    return out << t.elapsed_().count() << "ms";
+  }
+
+private:
+  typedef std::chrono::high_resolution_clock clock_;
+  std::chrono::high_resolution_clock::time_point start_;
+
+  void reset_() {
+    start_ = clock_::now();
+  }
+
+  std::chrono::milliseconds elapsed_() const {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+      clock_::now() - start_);
+  }
+};
+
+auto main() -> decltype(0)
+{
+  timer t;
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+  std::cout << t << '\n';
+}
