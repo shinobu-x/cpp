bb33b172c25412fba1eba5d526c49ed302e56b7c
Shinobu Kinjo <shinobu@redhat.com>
2017-07-15 13:27:41 +0900

    new file:   test/bit/bit_v1.cpp

---
+++ test/bit/bit_v1.cpp
@@ -0,0 +1,33 @@
+#include <x86intrin.h>
+
+#include <iostream>
+
+/* -march=native -mtune=native */
+
+auto main() -> decltype(0)
+{
+  // A number of 1
+  std::cout << _popcnt64(0x00) << '\n';
+  std::cout << _popcnt64(0x01) << '\n';
+  std::cout << _popcnt64(0x55) << '\n';
+  std::cout << _popcnt64(0xffff0000) << '\n';
+  std::cout << _popcnt64(0xffffffff) << '\n';
+
+  std::cout << "******" << '\n';
+
+  // A number of 0 from LSb / MSb
+  std::cout << _tzcnt_u64(0x00) << '\n';
+  std::cout << _tzcnt_u64(0x01) << '\n';
+  std::cout << _tzcnt_u64(0x55) << '\n';
+  std::cout << _tzcnt_u64(0xffff0000) << '\n';
+  std::cout << _tzcnt_u64(0xffffffff) << '\n';
+
+  std::cout << "******" << '\n';
+
+  std::cout << _lzcnt_u64(0x00) << '\n';
+  std::cout << _lzcnt_u64(0x01) << '\n';
+  std::cout << _lzcnt_u64(0x55) << '\n';
+  std::cout << _lzcnt_u64(0xffff0000) << '\n';
+  std::cout << _lzcnt_u64(0xffffffff) << '\n';
+
+}
