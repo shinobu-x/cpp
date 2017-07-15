9e0864d91f1ed25e8c6b61d4ffdfada44c53091c
Shinobu Kinjo <shinobu@redhat.com>
2017-07-16 08:28:30 +0900

    modified:   std/enable_if/enable_if_v1.cpp

--- std/enable_if/enable_if_v1.cpp
+++ std/enable_if/enable_if_v1.cpp
@@ -5,9 +5,9 @@ extern void* enabler;
 
 // template <typename T>
 struct selector_t {
-
+/*
   template <typename T, typename std::enable_if<
-    std::is_same<T, float>::value>::type* = nullptr>
+    std::is_same<T, float>::value>::type*& = enabler>
   void type_f() {
     std::cout << "Type float" << '\n';
   }
@@ -16,14 +16,30 @@ struct selector_t {
     std::is_same<T, double>::value>::type* = nullptr>
   void type_f() {
     std::cout << "Type double" << '\n';
-  } 
+  }
+*/
+  template <typename T>
+  typename std::enable_if<std::is_same<T, double>::value>::type type_f() {
+    std::cout << "Type double" << '\n';
+  }
+
+  template <typename T>
+  typename std::enable_if<std::is_same<T, float>::value>::type type_f() {
+    std::cout << "Type float" << '\n';
+  }
+
+  template <typename T>
+  typename std::enable_if<std::is_same<T, int>::value>::type type_f() {
+    std::cout << "Type int" << '\n';
+  }
 };
 
 auto main() -> decltype(0)
 {
   selector_t t;
   t.type_f<float>();
-  t.type_f<double>();  
+  t.type_f<double>();
+  t.type_f<int>();
   return 0;
 }
 
