984e0b31b59ae93d16866487676c0530ded1a66d
Shinobu Kinjo <shinobu@redhat.com>
2017-07-24 08:24:25 +0900

    modified:   boost/asio/impl/hpp/spawn.hpp

--- boost/asio/impl/hpp/spawn.hpp
+++ boost/asio/impl/hpp/spawn.hpp
@@ -26,5 +26,34 @@ private:
   boost::system::error_code* ec_;
 };
 
+struct is_continuation_if_running {
+  template <typename Dispatcher, typename Handler>
+  bool operator() (Dispatcher& dispatcher, Handler&) const {
+    return dispatcher.running_in_this_thread();
+  }
+};
+
+/*
+typedef basic_yield_context<
+  boost::asio::detail::wrapped_handler<
+    boost::asio::io_service::strand, void(*)(),
+    is_continuation_if_running> > yield_context;
+*/
+template <typename Handler, typename Function>
+void spawn(BOOST_ASIO_MOVE_ARG(Function) function,
+  const boost::coroutines::attributes& attributes =
+    boost::coroutines::attributes());
+
+template <typename Function>
+void spawn(boost::asio::io_service::strand strand,
+  BOOST_ASIO_MOVE_ARG(Function) function,
+  const boost::coroutines::attributes& attributes =
+    boost::coroutines::attributes());
+
+template <typename Function>
+void spawn(boost::asio::io_service& io_service,
+  BOOST_ASIO_MOVE_ARG(Function) function,
+  const boost::coroutines::attributes& attributes =
+    boost::coroutines::attributes());
 #pragma once
 #include "../ipp/spawn.ipp"
