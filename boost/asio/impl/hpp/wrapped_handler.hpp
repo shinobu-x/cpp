bbce61510717a5ec06c7540128a5bab96b4d50bb
Shinobu Kinjo <shinobu@redhat.com>
2017-07-26 08:09:18 +0900

    new file:   boost/asio/impl/hpp/wrapped_handler.hpp

---
+++ boost/asio/impl/hpp/wrapped_handler.hpp
@@ -0,0 +1,7 @@
+// #include "handler_cont_helpers.hpp"
+#include <boost/asio/detail/bind_handler.hpp>
+#include <boost/asio/detail/handler_alloc_helpers.hpp>
+#include <boost/asio/detail/handler_cont_helpers.hpp>
+#include <boost/asio/detail/handler_invoke_helpers.hpp>
+
+
