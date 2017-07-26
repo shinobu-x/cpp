d921d5909848a29ebf11b944476293d185a1f3e5
Shinobu Kinjo <shinobu@redhat.com>
2017-07-27 08:04:42 +0900

    new file:   boost/asio/impl/hpp/strand.hpp

---
+++ boost/asio/impl/hpp/strand.hpp
@@ -0,0 +1,5 @@
+#include <boost/asio/detail/config.hpp>
+#include <boost/asio/detail/handler_type_requirements.hpp>
+#include <boost/asio/detail/strand_service.hpp>
+
+class boost::asio::io_service::strand {};
