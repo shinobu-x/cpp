ccf784f3b66bc873f07768f9f4019636290b887f
Shinobu Kinjo <shinobu@redhat.com>
2017-07-27 16:26:06 +0900

    new file:   boost/asio/impl/test/headers.cpp

---
+++ boost/asio/impl/test/headers.cpp
@@ -0,0 +1,10 @@
+#include "../hpp/allocator.hpp"
+#include "../hpp/coroutine.hpp"
+#include "../hpp/high_res_clock.hpp"
+#include "../hpp/unyield.hpp"
+#include "../hpp/yield.hpp"
+
+auto main() -> decltype(0)
+{
+  return 0;
+}
