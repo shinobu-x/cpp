6592bebf270c4fc5194d321020aaf5e42ffde6d0
Shinobu Kinjo <shinobu@redhat.com>
2017-07-28 08:24:52 +0900

    modified:   hpp/yield.hpp

--- boost/asio/impl/hpp/yield.hpp
+++ boost/asio/impl/hpp/yield.hpp
@@ -1,13 +1,13 @@
 #include "coroutine.hpp"
 
 #ifndef reenter
-# define reenter(c) BOOST_ASIO_CORO_REENTER(c)
+# define reenter(c) CORO_REENTER(c)
 #endif
 
 #ifndef yield
-# define yield BOOST_ASIO_CORO_YIELD
+# define yield CORO_YIELD
 #endif
 
 #ifndef fork
-# define fork BOOST_ASIO_CORO_FORK
+# define fork CORO_FORK
 #endif
