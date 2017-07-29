0abd393c71b08c536a4fa658c96d58df07642189
Shinobu Kinjo <shinobu@redhat.com>
2017-07-29 11:07:51 +0900

    new file:   boost/asio/impl/hpp/shared_ptr.hpp modified:   boost/asio/impl/hpp/tcp.hpp

---
+++ boost/asio/impl/hpp/shared_ptr.hpp
@@ -0,0 +1,9 @@
+#include <memory>
+
+namespace boost {
+namespace asio {
+namespace detail {
+using std::shared_ptr;
+} // detail
+} // asio
+} // boost
--- boost/asio/impl/hpp/tcp.hpp
+++ boost/asio/impl/hpp/tcp.hpp
@@ -8,6 +8,7 @@
 #include <boost/asio/ip/basic_resolver.hpp>
 #include <boost/asio/ip/basic_resolver_iterator.hpp>
 #include <boost/asio/ip/basic_resolver_query.hpp>
+#include <boost/asio/detail/push_options.hpp>
 
 class tcp {
 public:
@@ -17,8 +18,18 @@ public:
   typedef boost::asio::ip::basic_resolver<tcp> resolver;
   typedef boost::asio::basic_socket_iostream<tcp> iostream;
 
+#if defined(GENERATING_DOCUMENTATION)
   typedef boost::asio::detail::socket_option::boolean<
     IPPROTO_TCP, TCP_NODELAY> no_delay;
+#else
+#define BOOST_ASIO_OS_DEF_IPPROTO_TCP 6
+#define BOOST_ASIO_OS_DEF_TCP_NODELAY 0x1
+#define BOOST_ASIO_OS_DEF(c) BOOST_ASIO_OS_DEF_##c
+// #define BOOST_ASIO_OS_DEF_IPPROTO_TCP IPPROTO_TCP
+// #define BOOST_ASIO_OS_DEF_TCP_NODELAY TCP_NODELAY
+  typedef boost::asio::detail::socket_option::boolean<
+    BOOST_ASIO_OS_DEF(IPPROTO_TCP), BOOST_ASIO_OS_DEF(TCP_NODELAY)> no_delay;
+#endif
 
   static tcp v4() { return tcp(PF_INET); }
   static tcp v6() { return tcp(PF_INET6); }
