28488d1f4664a89486cef50fae07a05e53d8ca25
Shinobu Kinjo <shinobu@redhat.com>
2017-08-08 08:02:53 +0900

    modified:   test/async/future_fallback_to.cpp

--- test/async/future_fallback_to.cpp
+++ test/async/future_fallback_to.cpp
@@ -1,36 +1,28 @@
-#include <boost/thread/detail/log.hpp>
-
 #include <cassert>
 #include <exception>
 #include <future>
-#include <string>
 #include <iostream>
+#include <string>
+#include <utility>
 
 #include "../macro/config.hpp"
 
 const unsigned number_of_tests = 200;
 
 int func_ex() {
-  BOOST_THREAD_LOG << __func__ << BOOST_THREAD_END_LOG;
   throw std::logic_error("Error");
 }
 
 int func() {
-  BOOST_THREAD_LOG << __func__ << BOOST_THREAD_END_LOG;
   return 1;
 }
 
 void test_1() {
-  std::cout << __func__ << '\n';
-
   for (unsigned i = 0; i < number_of_tests; ++i)
     try {
-      BOOST_THREAD_LOG << "" << BOOST_THREAD_END_LOG;
       std::future<int> f1 = std::async(std::launch::async, &func);
-      BOOST_THREAD_LOG << "" << BOOST_THREAD_END_LOG;
       f1.wait();
       assert(f1.get() == 1);
-      BOOST_THREAD_LOG << "" << BOOST_THREAD_END_LOG;
     } catch (std::exception& e) {
       LOG;
       std::cout << e.what() << '\n';
@@ -40,7 +32,25 @@ void test_1() {
       return;
     }
 }
+
+void test_2() {
+  for (unsigned i = 0; i < number_of_tests; ++i)
+    try {
+      std::future<int> f1 = std::async(&func);
+      std::future<int> f2 = std::move(f1);
+      f2.wait();
+      assert(f2.get() == 1);
+    } catch (std::exception& e) {
+      LOG;
+      std::cout << e.what() << '\n';
+      return;
+    } catch (...) {
+      LOG;
+      return;
+    }
+}
+
 auto main() -> decltype(0) {
-  test_1();
+  test_1(); test_2();
   return 0;
 }
