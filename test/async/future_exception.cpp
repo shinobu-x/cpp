367254175fea21ab1d50b4d1c08f3d5598c8013b
Shinobu Kinjo <shinobu@redhat.com>
2017-08-08 15:28:12 +0900

    new file:   test/async/future_exception.cpp

---
+++ test/async/future_exception.cpp
@@ -0,0 +1,85 @@
+#include <cassert>
+#include <exception>
+#include <future>
+#include <iostream>
+#include <string>
+#include <utility>
+
+#include "../macro/config.hpp"
+
+const unsigned number_of_tests = 200;
+
+int func_ex() {
+  throw std::logic_error("Error");
+}
+
+int func() {
+  return 1;
+}
+
+void test_1() {
+  for (unsigned i = 0; i < number_of_tests; ++i)
+    try {
+      std::future<int> f1 = std::async(std::launch::async, &func);
+      f1.wait();
+      assert(f1.get() == 1);
+    } catch (std::exception& e) {
+      LOG;
+      std::cout << e.what() << '\n';
+      return;
+    } catch (...) {
+      LOG;
+      return;
+    }
+}
+
+void test_2() {
+  for (unsigned i = 0; i < number_of_tests; ++i)
+    try {
+      std::future<int> f1 = std::async(&func);
+      std::future<int> f2 = std::move(f1);
+      f2.wait();
+      assert(f2.get() == 1);
+    } catch (std::exception& e) {
+      LOG;
+      std::cout << e.what() << '\n';
+      return;
+    } catch (...) {
+      LOG;
+      return;
+    }
+}
+
+void test_3() {
+  for (unsigned i = 0; i < number_of_tests; ++i)
+    try {
+      std::future<int> f1 = std::async(std::launch::async, &func_ex);
+      f1.wait();
+      assert(f1.get() == -1);
+    } catch (std::exception& e) {
+      std::cout << e.what() << '\n';
+    } catch (...) {
+      LOG;
+      return;
+    }
+}
+
+void test_4() {
+  for (unsigned i = 0; i < number_of_tests; ++i)
+    try {
+      std::future<int> f1 = std::async(std::launch::async, &func_ex);
+      std::future<int> f2 = std::move(f1);
+      f2.wait();
+      assert(f2.get() == -1);
+    } catch (std::exception& e) {
+      std::cout << e.what() << '\n';
+    } catch (...) {
+      LOG;
+      return;
+    }
+}
+
+auto main() -> decltype(0) {
+  test_1(); test_2();  test_3(); test_4(); 
+  return 0;
+}
