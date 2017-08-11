edf865aca19904322dc263efe53911f54bafaa63
Shinobu Kinjo <shinobu@redhat.com>
2017-08-11 15:53:32 +0900

    modified:   boost/asio/test/deadline_timer.cpp

--- boost/asio/test/deadline_timer.cpp
+++ boost/asio/test/deadline_timer.cpp
@@ -33,7 +33,7 @@ void cancel_timer(boost::asio::deadline_timer* t) {
   assert(num_cancelled == 1);
 }
 
-void cancle_one_timer(boost::asio::deadline_timer* t) {
+void cancel_one_timer(boost::asio::deadline_timer* t) {
   std::size_t num_cancelled = t->cancel_one();
   assert(num_cancelled == 1);
 }
@@ -152,7 +152,46 @@ void deadline_timer_test() {
   end = now();
   expected_end = start + boost::posix_time::seconds(2);
   assert(end < expected_end);
+
+  // Wait on the timer again without cancelling it. This time the asynchronous
+  // wait should run to completion and increment the counter.
+  t5.async_wait(boost::bind(increment_if_not_cancelled, &count,
+    boost::asio::placeholders::error));
+
+  ios.reset();
+  ios.run();
+
+  // The timer should not have been cancelled, so count should have changed.
+  // The total time since the timer was created should be more than 10 seconds.
+  assert(count == 1);
+  end = now();
+  expected_end = start + boost::posix_time::seconds(10);
+  assert(expected_end < end || expected_end == end);
+
+  count = 0;
+  start = now();
+
+  // Start two waits on a timer, one of which will be cancelled. The one which
+  // is not cancelled should still run to completion and increment the counter.
+  boost::asio::deadline_timer t7(ios, boost::posix_time::seconds(3));
+  t7.async_wait(boost::bind(increment_if_not_cancelled, &count,
+    boost::asio::placeholders::error));
+  t7.async_wait(boost::bind(increment_if_not_cancelled, &count,
+    boost::asio::placeholders::error));
+  boost::asio::deadline_timer t8(ios, boost::posix_time::seconds(1));
+  t8.async_wait(boost::bind(cancel_one_timer, &t7));
+
+  ios.reset();
+  ios.run();
+
+  // One of the waits should not have been cancelled, so count have changed.
+  // The total time since the timer was created should be more than 3 seconds.
+  assert(count == 3);
+  end = now();
+  expected_end = start + boost::posix_time::seconds(3);
+  assert(expected_end < end || expected_end == end);
 }
+
 auto main() -> decltype(0) {
   return 0;
 }
