a095978553153afc0ae2611cf5def41528ac49ef
Shinobu Kinjo <shinobu@redhat.com>
2017-08-14 07:39:17 +0900

    new file:   test/condition_variable/test_condition_notify_all.cpp new file:   test/thread/condition_test_common.hpp

---
+++ test/condition_variable/test_condition_notify_all.cpp
@@ -0,0 +1,10 @@
+#include <boost/bind.hpp>
+#include <boost/thread.hpp>
+
+#include "../thread/condition_test_common.hpp"
+
+unsigned const number_of_threads = 5;
+
+auto main() -> decltype(0) {
+  return 0;
+}
---
+++ test/thread/condition_test_common.hpp
@@ -0,0 +1,75 @@
+#include <boost/thread/condition_variable.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread_time.hpp>
+
+unsigned const timeout_seconds = 5;
+
+struct wait_for_flag {
+  boost::mutex m_;
+  boost::condition_variable cond_;
+  bool flag_;
+  unsigned woken_;
+
+  wait_for_flag()
+    : flag_(false), woken_(0) {}
+
+  struct check_flag {
+    check_flag(bool const& flag)
+      : flag_(flag) {}
+
+    bool operator()() const {
+      return flag_;
+    }
+  private:
+    bool const& flag_;
+    void operator=(check_flag&);
+  };
+
+  void wait_without_predicate() {
+    boost::unique_lock<boost::mutex> l(m_);
+
+    while (!flag_)
+      cond_.wait(l);
+
+    ++woken_;
+  }
+
+  void wait_with_predicate() {
+    boost::unique_lock<boost::mutex> l(m_);
+    cond_.wait(l, check_flag(flag_));
+
+    if (flag_)
+      ++woken_;
+  }
+
+  void timed_wait_without_predicate() {
+    boost::system_time const timeout =
+      boost::get_system_time() + boost::posix_time::seconds(timeout_seconds);
+    boost::unique_lock<boost::mutex> l(m_);
+
+    while (!flag_)
+      if (!cond_.timed_wait(l, timeout))
+        return;
+
+    ++woken_;
+  }
+
+  void timed_wait_with_predicate() {
+    boost::system_time const timeout =
+      boost::get_system_time() + boost::posix_time::seconds(timeout_seconds);
+    boost::unique_lock<boost::mutex> l(m_);
+
+    if (cond_.timed_wait(l, timeout, check_flag(flag_)) && flag_)
+      ++woken_;
+
+  }
+
+  void relative_timed_wait_with_predicate() {
+    boost::unique_lock<boost::mutex> l(m_);
+
+    if (cond_.timed_wait(l, boost::posix_time::seconds(timeout_seconds),
+      check_flag(flag_)) && flag_)
+      ++woken_;
+  }
+};
+
