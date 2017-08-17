5817d608fbacf6d396806893f14c0d43d65ff9a1
Shinobu Kinjo <shinobu@redhat.com>
2017-08-18 07:56:17 +0900

    new file:   test/container/map/dummy_test_allocator.hpp new file:   test/container/map/emplace_test.hpp new file:   test/container/map/map_test.cpp new file:   test/container/map/map_test.hpp new file:   test/container/map/movable_int.hpp new file:   test/container/map/print_container.hpp new file:   test/container/map/propagate_allocator_test.hpp new file:   test/intrusive/iterator_test.hpp

---
+++ test/container/map/map_test.cpp
@@ -0,0 +1,17 @@
+#include <boost/container/detail/config_begin.hpp>
+#include <boost/container/map.hpp>
+#include <boost/container/adaptive_pool.hpp>
+
+#include <map>
+
+#include "print_container.hpp"
+#include "movable_int.hpp"
+#include "dummy_test_allocator.hpp"
+#include "map_test.hpp"
+#include "propagate_allocator_test.hpp"
+#include "emplace_test.hpp"
+#include "../../intrusive/iterator_test.hpp"
+
+auto main() -> decltype(0) {
+  return 0;
+}
