1b51af54f407c437d11c275b4cd4fb0e6d4f50fb
Shinobu Kinjo <shinobu@redhat.com>
2017-08-21 08:03:52 +0900

    modified:   test/container/map/map_test.hpp

--- test/container/map/map_test.hpp
+++ test/container/map/map_test.hpp
@@ -231,6 +231,156 @@ namespace {
       if (!check_equal_containers(*ptr_boost_multimap, *ptr_std_multimap))
         return 1;
     }
-          
+    ::boost::movelib::unique_ptr<boost_map> const ptr_boost_map =
+      ::boost::movelib::make_unique<boost_map>();
+    ::boost::movelib::unique_ptr<std_map> const ptr_std_map =
+      ::boost::movelib::make_unique<str_map>();
+    ::boost::movelib::unique_ptr<boost_multimap> const ptr_boost_map =
+      ::boost::movelib::make_unique<boost_multimap>();
+    ::boost::movelib::unique_ptr<std_multimap> const ptr_std_multimap =
+      ::boost::movelib::make_unique<std_multimap>();
+
+    boost_map& boost_map_t = *ptr_boost_map;
+    std_map& std_map_t = *ptr_std_map;
+    boost_multimap& boost_multimap_t = *ptr_boost_multimap;
+    std_multimap& std_multimap_t = *ptr_std_multimap;
+
+    {
+      int_pair_type aux_vect1[max_elem];
+      for (int i = 0; i < max_elem; ++i) {
+        int_type i1(i/2);
+        int_type i2(i/2);
+        new(&aux_vect1[i])int_pair_type(boost::move(i1), boost::move(i2));
+      }
+
+      typedef typename std_map::value_type std_value_type;
+      typedef typename std_map::key_type std_key_type;
+      typedef typename std_map::mapped_key std_mapped_type;
+      std_value_type aux_vect2[max_elem];
+      for (int i = 0; i < max_elem; ++i)
+        new(&aux_vect2[i])std_value_type(boost::move(i1), boost::move(i2));
+
+      int_pair_type aux_vect3[max_elem];
+      for (int i = 0; i < max_elem; ++i) {
+        int_type i1(i/2);
+        int_type i2(i/2);
+        new(&aux_vect3[i])int_pair_type(boost::move(i1), boost::move(i2));
+      }
+
+      ::boost::movelib::unique_ptr<boost_map> const ptr_boost_map2 =
+        ::boost::movelib::make_unique<boost_map>(
+          boost::make_move_iterator(&aux_vect1[0]),
+          boost::make_move_iterator(&aux_vect1[0] + max_elem));
+
+      ::boost::movelib::unique_ptr<std_map> const ptr_std_map2 =
+        ::boost::movelib::make_unique<std_map>(
+          &aux_vect2[0],
+          &aux_vect2[0] + max_elem);
+
+      ::boost::movelib::unique_ptr<boost_multimap> const ptr_boost_multimap2 =
+        ::boost::movelib::make_unique<boost_multimap>(
+          boost::make_move_iterator(&aux_vect3[0]),
+          boost::make_move_iterator(&aux_vect3[0] + max_elem));
+
+      ::boost::movelib::unique_ptr<std_multimap> const ptr_std_multimap2  =
+        ::boost::movelib::make_unique<std_multimap>(
+          &aux_vect2[0],
+          &aux_vect2[0] + max_elem);
+
+      boost_map& boost_map_t2 = *ptr_boost_map2;
+      std_map& std_map_t2 = *ptr_std_map2;
+      boost_multimap& boost_multimap_t2 = *ptr_boost_multimap2;
+      std_multimap& std_multimap_t2 = *ptr_std_multimap2;
+
+      if (!check_equal_containers(boost_map_t2, std_map_t2))
+        return 1;
+
+      if (!check_equal_containers(boost_multimap_t2, std_multimap_t2))
+        return 1;
+
+      for (int i = 0; i < max_elem; ++i) {
+        int_type i1(i);
+        int_type i2(i);
+        new(&aux_vect1[i])int_pair_type(boost::move(i1), boost::move(i2));
+      }
+
+      for (int i = 0; i < max_elem; ++i) {
+        new(&aux_vect2[i])std_value_type(std_key_type(i), std_mapped_type(i));
+
+      for (int i = 0; i < max_elem; ++i) {
+        int_type i1(i);
+        int_type i2(i);
+        new(&aux_vect3[i])int_pair_type(boost::move(i1), boost::move(i2));
+      }
+
+      if (!check_equal_containers(boost_map_t2, std_map_t2))
+        return 1;
+
+      if (!check_equal_containers(boost_multimap_t2, std_multimap_t2))
+        return 1;
+
+      if (!(boost_map_t2 == boost_map_t2))
+        return 1;
+
+      if (boost_map_t2 != boost_map_t2)
+        return 1;
+
+      if (boost_map_t2 < boost_map_t2)
+        return 1;
+
+      if (boost_map_t2 > boost_map_t2)
+        return 1;
+
+      if (!(boost_map_t2 <= boost_map_t2))
+        return 1;
+
+      if (!(boost_map_t2 >= boost_map_t2))
+        return 1;
+
+      ::boost::movelib::unique_ptr<boost_map> const ptr_boost_map3 =
+        ::boost::movelib::make_unique<boost_map>(
+          boost::make_move_iterator(&aux_vect1[0]),
+          boost::make_move_iterator(&aux_vect1[0] + max_elem));
+
+      ::boost::movelib::unique_ptr<std_map> const ptr_std_map3 =
+        ::boost::movelib::make_unique<std_map>(
+          &aux_vect2[0],
+          &aux_vect2[0] + max_elem);
+
+      ::boost::movelib::unique_ptr<boost_multimap> const ptr_boost_multimap3 =
+        ::boost::movelib::make_unique<boost_multimap>(
+          boost::make_move_iterator(&aux_vect3[0]),
+          boost::make_move_iterator(&aux_vect3[0] + max_elem));
+
+      ::boost::movelib::unique_ptr<std_multimap> const ptr_std_multimap3 =
+        ::boost::movelib::make_unique<std_multimap>(
+          &aux_vect2[0],
+          &aux_vect2[0] + max_elem);
+
+      boost_map& boost_map_t3 = *ptr_boost_map3;
+      std_map& std_map_t3 = *ptr_std_map3;
+      boost_multimap& boost_multimap_t3 = *ptr_boost_multimap3;
+      std_multimap& std_multimap_t3 = *ptr_std_multimap3;
+
+      if (!check_equal_containers(boost_map_t3, std_map_t3))
+        return 1;
+
+      if (!check_equal_containers(boost_multimap_t3, std_multimap_t3))
+        return 1;
+
+      {
+        int_type i0(0);
+        boost_map_t2.erase(i0);
+        boost_multimap_t2.erase(i0);
+        std_map_t2.erase(0);
+        std_multimap_t2.erase(0);
+      }
+      {
+        int_type i0(0);
+        int_type i1(0);
+        boost_map_t2[::boost::move(i0)] = ::boost::move(i1);
+      }
+      {
+   
       
 } // namespace
