fd5c1fa720f800bd1b6aca1838aa7b1ff1c0a724
Shinobu Kinjo <shinobu@redhat.com>
2017-08-24 03:37:37 +0900

    modified:   test/container/map/map_test.cpp

--- test/container/map/map_test.cpp
+++ test/container/map/map_test.cpp
@@ -12,6 +12,50 @@
 #include "emplace_test.hpp"
 #include "../../intrusive/iterator_test.hpp"
 
+typedef std::pair<movable_and_copyable_int, movable_and_copyable_int> pair_t;
+
+namespace boost { namespace container {
+  template class map <
+    movable_and_copyable_int,
+    movable_and_copyable_int,
+    std::less<movable_and_copyable_int>
+    /* simple_allocator<pair_t>*/ >;
+
+
+/*  template class map <
+    movable_and_copyable_int,
+    movable_and_copyable_int,
+    std::less<movable_and_copyable_int>
+    boost::container::adaptive_pool<pair_t> >; */
+
+  template class multimap <
+    movable_and_copyable_int,
+    movable_and_copyable_int,
+    std::less<movable_and_copyable_int>
+    /* std::allocator<pair_t> */>;
+}}
+
+class recursive_map;
+class recursive_multimap;
+struct get_allocator_map;
+struct boost_container_map;
+struct boost_container_multimap;
+
+
+namespace {
+template <>
+struct alloc_propagate_base<boost_container_map>{
+  template <class T, class allocator_t>
+  struct apply {
+    typedef typename boost::container::allocator_traits<allocator_t>::
+      template portable_rebind_alloc<
+        std::pair<const T, T> >::type alloc_t;
+
+    typedef boost::container::multimap<T, T, std::less<T>, alloc_t> type;
+  };
+};
+} // namespace
+
 auto main() -> decltype(0) {
   return 0;
 }
