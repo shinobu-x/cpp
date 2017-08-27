47078460dc4ac0b35a991cfa0e758c12082e6c4a
Shinobu Kinjo <shinobu@redhat.com>
2017-08-28 08:23:08 +0900

    modified:   test/mutex/test_shared_mutex.cpp

--- test/mutex/test_shared_mutex.cpp
+++ test/mutex/test_shared_mutex.cpp
@@ -1,4 +1,55 @@
+#include <boost/thread/thread.hpp>
+#include <boost/thread/xtime.hpp>
+
 #include "hpp/shared_mutex_locking_thread.hpp"
+
+#define CHECK_LOCKED_VALUE_EQUAL(mutex_name, value, expected_value ) { \
+  boost::unique_lock<boost::mutex> l(mutex_name);                      \
+  BOOST_CHECK_EQUAL(value, expected_value);                            \
+}
+
+// Test multiple readers
+void test_1() {
+  unsigned const number_of_threads = 10;
+  boost::thread_group threads;
+  shared_mutex rwm_mutex;
+  unsigned unblocked_count = 0;
+  unsigned simultaneous_running_count = 0;
+  unsigned max_simultaneous_running = 0;
+  boost::mutex unblocked_count_mutex;
+  boost::condition_variable unblocked_condition;
+  boost::mutex finish_mutex;
+  boost::unique_lock<boost::mutex> finsih_lock(finish_mutex);
+
+  try {
+    for (unsigned i = 0; i < number_of_threads; ++i)
+      threads.create_thread(
+        locking_thread<boost::shared_lock<shared_mutex> > (
+          rwm_mutex, unblocked_count, unblocked_count_mutex,
+          unblocked_condition, finish_mutex, simultaneous_running_count,
+          max_simultaneous_running));
+
+    {
+      boost::unique_lock<boost::mutex> unblocked_count(unblocked_count_mutex);
+      while (unblocked_count < number_of_threads)
+        unblocked_condition.wait(unblocked_count_mutex);
+    }
+
+    CHECK_LOCKED_VALUE_EQUAL(
+      unblocked_count_mutex, unblocked_count, number_of_threads);
+
+    finish_lock.unlock();
+
+    threads.join_all();
+  } catch (...) {
+    threads.interrupt_all();
+    threads.join_all();
+    throw;
+  }
+
+  CHECK_LOCKED_VALUE_EQUAL(
+    unblocked_count_mutex, max_simultaneous_running, number_of_threads);
+}
 
 auto main() -> decltype(0) {
   return 0;
