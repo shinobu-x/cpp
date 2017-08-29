a7d39b48dbf9e009163e359814cbb3e6a34a2e4d
Shinobu Kinjo <shinobu@redhat.com>
2017-08-30 07:45:49 +0900

    modified:   test/mutex/test_shared_mutex.cpp

--- test/mutex/test_shared_mutex.cpp
+++ test/mutex/test_shared_mutex.cpp
@@ -231,10 +231,89 @@ void test_5() {
   unsigned unblocked_count = 0;
   unsigned simultaneous_running_readers = 0;
   unsigned max_simultaneous_readers = 0;
-  unsigned simultaneous_writers = 0;
+  unsigned simultaneous_running_writers = 0;
   unsigned max_simultaneous_writers = 0;
-  boost::mutex 
+  boost::mutex unblocked_count_mutex;
+  boost::condition_variable unblocked_cond;
+  boost::mutex finish_reading_mutex;
+  boost::unique_lock <boost::mutex> finish_reading_lock(finish_reading_mutex);
+  boost::mutex finish_writing_mutex;
+  boost::unique_lock<boost::mutex> finish_writing_lock(finish_writing_mutex);
+  unsigned const reader_count = 10;
+  unsigned const writer_count = 10;
+
+  try {
+    for (unsigned i = 0; i < reader_count; ++i)
+      threads.create_thread(
+        locking_thread<boost::shared_lock<shared_mutex> >(
+          rwm_mutex,
+          unblocked_count,
+          unblocked_count_mutex,
+          unblocked_cond,
+          finish_reading_mutex,
+          simultaneous_running_readers,
+          max_simultaneous_readers));
+
+    {
+      boost::thread::sleep(delay(1));
+    }
+
+    for (unsigned i = 0; i < writer_count; ++i) 
+      threads.create_thread(
+        locking_thread<boost::unique_lock<shared_mutex> >(
+          rwm_mutex,
+          unblocked_count,
+          unblocked_count_mutex,
+          unblocked_cond,
+          finish_writing_mutex,
+          simultaneous_running_writers,
+          max_simultaneous_writers));
+
+    {
+      boost::unique_lock<boost::mutex> unblocked_lock(unblocked_count_mutex);
+      while (unblocked_count < reader_count)
+        unblocked_cond.wait(unblocked_lock);
+    }
+
+    {
+     boost::thread::sleep(delay(1));
+      CHECK_LOCKED_VALUE_EQUAL(
+        unblocked_count_mutex, unblocked_count, reader_count);
+    }
+
+    finish_reading_lock.unlock();
+
+    {
+      boost::unique_lock<boost::mutex> unblocked_lock(unblocked_count_mutex);
+      while (unblocked_count < (reader_count+1))
+        unblocked_cond.wait(unblocked_lock);
+
+    }
+
+    CHECK_LOCKED_VALUE_EQUAL(
+      unblocked_count_mutex, unblocked_count, reader_count+1);
+
+    finish_writing_lock.unlock();
+    threads.join_all();
+  } catch (...) {
+    threads.interrupt_all();
+    threads.join_all();
+    throw;
+  }
+
+
+  CHECK_LOCKED_VALUE_EQUAL(
+    unblocked_count_mutex, unblocked_count, reader_count+writer_count);
+
+  CHECK_LOCKED_VALUE_EQUAL(
+    unblocked_count_mutex, max_simultaneous_readers, reader_count);
+
+  CHECK_LOCKED_VALUE_EQUAL(
+    unblocked_count_mutex, max_simultaneous_writers, 1u);
+
+}
+
 auto main() -> decltype(0) {
-  test_1(); test_2(); test_3(); test_4();
+  test_1(); test_2(); test_3(); test_4(); test_5();
   return 0;
 }
