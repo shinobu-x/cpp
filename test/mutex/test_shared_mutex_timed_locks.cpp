6d9e6ed36f88ad692919608e48b25f6aad9f0521
Shinobu Kinjo <shinobu@redhat.com>
2017-08-31 16:07:53 +0900

    modified:   test/mutex/test_shared_mutex_timed_locks.cpp

--- test/mutex/test_shared_mutex_timed_locks.cpp
+++ test/mutex/test_shared_mutex_timed_locks.cpp
@@ -21,7 +21,7 @@ void test_1() {
   boost::thread writer(
     simple_writing_thread(
       rwm_mutex, finish_mutex, unblocked_mutex, unblocked_count));
-  boost::thread::sleep(delay());
+  boost::thread::sleep(delay(1));
 
   CHECK_LOCKED_VALUE_EQUAL(unblocked_mutex, unblocked_count, 1u);
 
@@ -56,7 +56,74 @@ void test_1() {
   writer.join(); 
 }
 
+// Test timed lock shared succeeds if no lock held
+void test_2() {
+  shared_mutex rwm_mutex;
+  boost::mutex finish_mutex;
+  boost::mutex unblocked_mutex;
+
+  boost::system_time const start = boost::get_system_time();
+  boost::system_time const timeout1 =
+    start + boost::posix_time::milliseconds(500);
+  boost::posix_time::milliseconds const timeout_resolution(50);
+  bool timed_lock_succeeded = rwm_mutex.timed_lock_shared(timeout1);
+
+  assert(boost::get_system_time() < timeout1);
+  assert(timed_lock_succeeded);
+
+  if (timed_lock_succeeded)
+    rwm_mutex.unlock_shared();
+
+  boost::posix_time::milliseconds const wait_duration(500);
+  boost::system_time const timeout2 = boost::get_system_time() + wait_duration;
+  timed_lock_succeeded = rwm_mutex.timed_lock_shared(wait_duration);
+
+  assert(boost::get_system_time() < timeout2);
+  assert(timed_lock_succeeded);
+
+  if (timed_lock_succeeded)
+    rwm_mutex.unlock_shared();
+}
+
+// Test timed lock shared succeeds if read lock held
+void test_3() {
+  shared_mutex rwm_mutex;
+  boost::mutex finish_mutex;
+  boost::mutex unblocked_mutex;
+  unsigned unblocked_count = 0;
+  boost::unique_lock<boost::mutex> finish_lock(finish_mutex);
+  boost::thread reader(simple_reading_thread(
+    rwm_mutex, finish_mutex, unblocked_mutex, unblocked_count));
+  boost::thread::sleep(delay(1));
+
+  CHECK_LOCKED_VALUE_EQUAL(unblocked_mutex, unblocked_count, 1u);
+
+  boost::system_time const start = boost::get_system_time();
+  boost::system_time const timeout1 =
+    start + boost::posix_time::milliseconds(500);
+  bool timed_lock_succeeded = rwm_mutex.timed_lock_shared(timeout1);
+
+  assert(boost::get_system_time() < timeout1);
+  assert(timed_lock_succeeded);
+
+  if (timed_lock_succeeded)
+    rwm_mutex.unlock_shared();
+
+  boost::posix_time::milliseconds const wait_duration(500);
+  boost::system_time const timeout2 = boost::get_system_time() + wait_duration;
+  timed_lock_succeeded = rwm_mutex.timed_lock_shared(wait_duration);
+
+  assert(boost::get_system_time() < timeout2);
+  assert(timed_lock_succeeded);
+
+  if (timed_lock_succeeded)
+    rwm_mutex.unlock_shared();
+
+  finish_lock.unlock();
+  reader.join();
+}
+
 auto main() -> decltype(0) {
-  test_1();
+  // test_2()
   return 0;
 }
