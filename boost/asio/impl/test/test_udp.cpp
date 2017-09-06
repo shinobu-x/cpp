ce7484f40e96866c0b092e009043beae0807f5bd
Shinobu Kinjo <shinobu@redhat.com>
2017-09-06 15:46:18 +0900

    modified:   boost/asio/impl/test/test_udp.cpp

--- boost/asio/impl/test/test_udp.cpp
+++ boost/asio/impl/test/test_udp.cpp
@@ -18,7 +18,7 @@ void test_1() {
   try {
     boost::asio::io_service ios;
     char mutable_char_buffer[128] = "";
-    const const_char_buffer[128] = "";
+    const char const_char_buffer[128] = "";
     boost::asio::socket_base::message_flags in_flags = 0;
     settable_socket_option<void> settable_socket_option1;
     settable_socket_option<int> settable_socket_option2;
@@ -28,26 +28,66 @@ void test_1() {
     gettable_socket_option<double> gettable_socket_option3;
     io_control_command io_control_command;
     lazy_handler lazy;
-    boost::sysem::error_code ec;
+    boost::system::error_code ec;
     boost::asio::ip::udp::socket s1(ios);
     boost::asio::ip::udp::socket s2(ios, boost::asio::ip::udp::v4());
     boost::asio::ip::udp::socket s3(ios,
       boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0));
     boost::asio::ip::udp::socket s4(ios,
       boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v6(), 0));
-#if !define(BOOST_ASIO_WINDOWS_RUNTIME)
+#if !defined(BOOST_ASIO_WINDOWS_RUNTIME)
     boost::asio::ip::udp::socket::native_handle_type native_socket1 =
       ::socket(AF_INET, SOCK_STREAM, IPPROTO_UDP);
     boost::asio::ip::udp::socket s6(ios,
       boost::asio::ip::udp::v4(), native_socket1);
 #endif
-#if define(BOOST_ASIO_HAS_MOVE)
-   boost::asio::ip::udp::socket s7(std::move(s6));
-   s1 = boost::asio::ip::udp::socket(ios);
-   s1 = std::move(s2);
+#if defined(BOOST_ASIO_HAS_MOVE)
+    boost::asio::ip::udp::socket s7(std::move(s6));
+    s1 = boost::asio::ip::udp::socket(ios);
+    s1 = std::move(s2);
 #endif
-   boost::asio::io_service& ios_ref = s1.get_io_service();
-
+    boost::asio::io_service& ios_ref = s1.get_io_service();
+    (void)ios_ref;
+    boost::asio::ip::udp::socket::lowest_layer_type& lowest_layer1 =
+      s1.lowest_layer();
+    (void)lowest_layer1;
+    const boost::asio::ip::udp::socket& s8 = s1;
+    const boost::asio::ip::udp::socket::lowest_layer_type& lowest_layer2 =
+      s8.lowest_layer();
+    (void)lowest_layer2;
+    s1.open(boost::asio::ip::udp::v4());
+    s1.open(boost::asio::ip::udp::v6());
+    s1.open(boost::asio::ip::udp::v4(), ec);
+    s1.open(boost::asio::ip::udp::v6(), ec);
+#if !defined(BOOST_ASIO_WINDOWS_RUNTIME)
+    boost::asio::ip::udp::socket::native_handle_type native_socket2 =
+      ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    s1.assign(boost::asio::ip::udp::v4(), native_socket2);
+    boost::asio::ip::udp::socket::native_handle_type native_socket3 =
+      ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    s1.assign(boost::asio::ip::udp::v4(), native_socket3, ec);
+#endif
+    bool is_open = s1.is_open();
+    (void)is_open;
+    s1.close();
+    s1.close(ec);
+    boost::asio::ip::udp::socket::native_type native_socket4 = s1.native();
+    (void)native_socket4;
+    boost::asio::ip::udp::socket::native_handle_type native_socket5 =
+      s1.native_handle();
+    (void)native_socket5;
+    s1.cancel();
+    s1.cancel(ec);
+    bool at_mark1 = s1.at_mark();
+    (void)at_mark1;
+    bool at_mark2 = s1.at_mark(ec);
+    (void)at_mark2;
+    std::size_t available1 = s1.available();
+    (void)available1;
+    std::size_t available2 = s1.available(ec);
+    (void)available2;
+    s1.bind(boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0));
+    s1.bind(boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v6(), 0));
   } catch (std::exception&) {}
 
 }
