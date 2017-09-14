ba2e517a41e77568df5ab3373d7b672a2d84c6ce
Shinobu Kinjo <shinobu@redhat.com>
2017-09-14 12:58:55 +0900

    new file:   test/thread/test_timed_lock_succeeds_if_no_lock_held.cpp

---
+++ test/thread/test_timed_lock_succeeds_if_no_lock_held.cpp
@@ -0,0 +1,50 @@
+#include <boost/thread/thread_only.hpp>
+#include <boost/thread/xtime.hpp>
+
+#include <cassert>
+
+#include "hpp/shared_mutex_locking_thread.hpp"
+
+struct test_timed_lock_succeeds_if_no_lock_held {
+  void operator()() {
+    shared_mutex rwm_mutex;
+    boost::mutex finish_mutex;
+    boost::mutex unblocked_mutex;
+    boost::system_time const start = boost::get_system_time();
+    boost::system_time const timeout1 =
+      start + boost::posix_time::milliseconds(500);
+    boost::posix_time::milliseconds const timeout_resolution(50);
+    bool timed_lock_succeeded = rwm_mutex.timed_lock(timeout1);
+
+    {
+//      assert(boost::get_system_time() < timeout1);
+//      assert(timed_lock_succeeded);
+    }
+
+    if (timed_lock_succeeded)
+      rwm_mutex.unlock();
+
+    boost::posix_time::milliseconds const wait_duration(500);
+    boost::system_time const timeout2 =
+      boost::get_system_time() + wait_duration;
+    timed_lock_succeeded = rwm_mutex.timed_lock(wait_duration);
+
+
+    {
+//      assert(boost::get_system_time() < timeout2);
+//      assert(timed_lock_succeeded);
+    }
+
+    if (timed_lock_succeeded)
+      rwm_mutex.unlock();
+  }
+};
+
+void do_test_timed_lock_succeeds_if_no_lock_held() {
+  test_timed_lock_succeeds_if_no_lock_held()();
+}
+
+auto main() -> decltype(0) {
+  do_test_timed_lock_succeeds_if_no_lock_held();
+  return 0;
+}
