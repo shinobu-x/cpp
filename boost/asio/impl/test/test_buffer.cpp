08131b02f339c253564c37e21427ec07c1e17ea1
Shinobu Kinjo <shinobu@redhat.com>
2017-09-18 03:02:46 +0900

    modified:   boost/asio/impl/test/test_buffer.cpp

--- boost/asio/impl/test/test_buffer.cpp
+++ boost/asio/impl/test/test_buffer.cpp
@@ -1,6 +1,8 @@
 #include <boost/asio/buffer.hpp>
 #include <boost/array.hpp>
 
+#include <array>
+
 void test_buffer() {
   try {
     char raw_data[1024];
@@ -9,6 +11,9 @@ void test_buffer() {
     boost::array<char, 1024> array_data;
     const boost::array<char, 1024>& const_array_data_1 = array_data;
     boost::array<const char, 1024> const_array_data_2 = {{0}};
+    std::array<char, 1024> std_array_data;
+    const std::array<char, 1024>& const_std_array_data_1 = std_array_data;
+    std::array<const char, 1024> const_std_array_2 = {{0}};
     std::vector<char> vector_data(1024);
     const std::vector<char>& const_vector_data = vector_data;
     const std::string string_data(1024, ' ');
@@ -63,4 +68,77 @@ void test_buffer() {
     (void)ptr1;
     const void* ptr2 = boost::asio::buffer_cast<const void*>(cb1);
     (void)ptr2;
+
+    mb1 = boost::asio::buffer(mb2);
+    mb1 = boost::asio::buffer(mb2, 128);
+    cb1 = boost::asio::buffer(cb2);
+    cb1 = boost::asio::buffer(cb2, 128);
+    mb1 = boost::asio::buffer(void_ptr_data, 1024);
+    cb1 = boost::asio::buffer(const_void_ptr_data, 1024);
+    mb1 = boost::asio::buffer(raw_data);
+    mb1 = boost::asio::buffer(raw_data, 1024);
+    cb1 = boost::asio::buffer(const_raw_data);
+    cb1 = boost::asio::buffer(const_raw_data, 1024);
+    mb1 = boost::asio::buffer(std_array_data);
+    mb1 = boost::asio::buffer(std_array_data, 1024);
+    cb1 = boost::asio::buffer(const_std_array_data_1);
+    cb1 = boost::asio::buffer(const_std_array_data_1, 1024);
+    cb1 = boost::asio::buffer(const_std_array_data_2);
+    cb1 = boost::asio::buffer(const_std_array_data_2, 1024);
+    mb1 = boost::asio::buffer(vector_data);
+    mb1 = boost::asio::buffer(vector_data, 1024);
+    cb1 = boost::asio::buffer(const_vector_data);
+    cb1 = boost::asio::buffer(const_vector_data, 1024);
+    cb1 = boost::asio::buffer(string_data);
+    cb1 = boost::asio::buffer(string_data, 1024);
+
+    std::size_t s7 = boost::asio::buffer_copy(mb1, cb2);
+    (void)s7;
+    std::size_t s8 = boost::asio::buffer_copy(mb1, cb2);
+    (void)s8;
+    std::size_t s9 = boost::asio::buffer_copy(mb1, mb2);
+    (void)s9;
+    std::size_t s10 = boost::asio::buffer_copy(mb1, mbc2);
+    (void)s10;
+    std::size_t s11 = boost::asio::buffer_copy(mb1, const_buffer_sequence);
+    (void)s11;
+    std::size_t s12 = boost::asio::buffer_copy(mbc1, cb2);
+    (void)s12;
+    std::size_t s13 = boost::asio::buffer_copy(mbc1, cbc2);
+    (void)s13;
+    std::size_t s14 = boost::asio::buffer_copy(mbc1, mb2);
+    (void)s14;
+    std::size_t s15 = boost::asio::buffer_copy(mbc1, mbc2);
+    (void)s15;
+    std::size_t s16 = boost::asio::buffer_copy(mbc1, const_buffer_sequence);
+    (void)s16;
+    std::size_t s17 = boost::asio::buffer_copy(mutable_buffer_sequence, cb2);
+    (void)s17;
+    std::size_t s18 = boost::asio::buffer_copy(mutable_buffer_sequence, cbc2);
+    (void)s18;
+    std::size_t s19 = boost::asio::buffer_copy(mutable_buffer_sequence, mb2);
+    (void)s19;
+    std::size_t s20 = boost::asio::buffer_copy(mutable_buffer_sequence, mbc2);
+    (void)s20;
+    std::size_t s21 =
+      boost::asio::buffer_copy(mutable_buffer_sequence, const_buffer_sequence);
+    (void)s21;
+    std::size_t s22 = boost::asio::buffer_copy(mb1, cb2, 128);
+    (void)s22;
+    std::size_t s23 = boost::asio::buffer_copy(mb1, cbc2, 128);
+    (void)s23;
+    std::size_t s24 = boost::asio::buufer_copy(mb1, mb2, 128);
+    (void)s24;
+    std::size_t s25 = boost::asio::buffer_copy(mb1, mbc2, 128);
+    (void)s25;
+    std::size_t s26 = boost::asio::buffer_copy(mb1, const_buffer_sequence, 128);
+    (void)s26;
+    std::size_t s27 = boost::asio::buffer_copy(mbc1, cb2, 128);
+    (void)s27;
+    std::size_t s28 = boost::asio::buffer_copy(mbc1, cbc2, 128);
+    (void)s28;
+    std::size_t s29 = boost::asio::buffer_copy(mbc1, mbc2, 128);
+    (void)s29;
+    std::size_t s30 = boost::asio::buffer_copy(mbc1, mbc2, 128);
+    (void)s30;
 
