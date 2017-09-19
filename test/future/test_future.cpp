1b203f1650c73fb2d44c565f9d8366f6851d5a68
Shinobu Kinjo <shinobu@redhat.com>
2017-09-20 03:31:24 +0900

    new file:   test/future/test_future.cpp

---
+++ test/future/test_future.cpp
@@ -0,0 +1,17 @@
+#define BOOST_THREAD_VERSION 4
+
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
+#include <boost/thread/future.hpp>
+
+#include <cassert>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "../thread/thread_move.hpp"
+#include "../thread/thread_only.hpp"
+
+auto main() -> decltype(0) {
+  return 0;
+}
