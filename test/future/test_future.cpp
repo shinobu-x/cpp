116779d3a3e98bfa6b55a84332d53d55ef6af1fd
Shinobu Kinjo <shinobu@redhat.com>
2017-09-21 03:06:02 +0900

    modified:   test/future/test_future.cpp

--- test/future/test_future.cpp
+++ test/future/test_future.cpp
@@ -1,8 +1,8 @@
 #define BOOST_THREAD_VERSION 4
 
-#include <boost/thread/mutex.hpp>
-#include <boost/thread/condition.hpp>
-#include <boost/thread/future.hpp>
+// #include <boost/thread/mutex.hpp>
+// #include <boost/thread/condition.hpp>
+// #include <boost/thread/future.hpp>
 
 #include <cassert>
 #include <memory>
@@ -10,7 +10,7 @@
 #include <utility>
 
 #include "../thread/thread_move.hpp"
-#include "../thread/thread_only.hpp"
+// #include "../thread/thread_only.hpp"
 
 auto main() -> decltype(0) {
   return 0;
