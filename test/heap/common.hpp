3079723c59300b1cee78df20e20fff2b3aed7b54
Shinobu Kinjo <shinobu@redhat.com>
2017-09-23 13:34:51 +0900

    modified:   test/heap/common.hpp

--- test/heap/common.hpp
+++ test/heap/common.hpp
@@ -28,8 +28,8 @@ std::vector<int> make_test_data(int size, int offset = 0, int strive = 1) {
   return r;
 }
 
-template <typename primary_queue, typename container_t>
-void check_que(primary_queue& que, container_t const& expected) {
+template <typename priority_queue, typename container_t>
+void check_que(priority_queue& que, container_t const& expected) {
   assert(que.size() == expected.size());
 
   for (unsigned int i = 0; i != expected.size(); ++i) {
@@ -41,38 +41,38 @@ void check_que(primary_queue& que, container_t const& expected) {
   assert(q.empty());
 }
 
-template <typename primary_queue, typename container_t>
-void fill_que(primary_queue& que, container_t const& data) {
+template <typename priority_queue, typename container_t>
+void fill_que(priority_queue& que, container_t const& data) {
   for (unsigned int i = 0; i != data.size(); ++i)
     que.push(data[i]);
 }
 
 #if !defined(BOOST_NO_CXX11_RVALUE_REFERENCE) && \
   !defined(BOOST_NO_CXX11_VARIADIC_TEMPLATE)
-template <typename primary_queue, template container_t>
-void fill_emplace_que(primary_queue& que, container_t const& data) {
+template <typename priority_queue, template container_t>
+void fill_emplace_que(priority_queue& que, container_t const& data) {
   for (unsigned int i = 0; i != data.size(); ++i) {
-    typename primary_queue::value_type value_t = data[i];
+    typename priority_queue::value_type value_t = data[i];
 
     que.emplace(std::move(value_t));
   }
 }
 #endif
 
-template <typename primary_queue>
+template <typename priority_queue>
 void test_sequential_push() {
   for (int i = 0; i != test_size; ++i) {
-    primary_queue que;
+    priority_queue que;
     std::vector<int> data = make_test_data(i);
     fill_que(que, data);
     check_que(que, data);
   }
 }
 
-template <typename primary_queue>
+template <typename priority_queue>
 void test_sequential_reverse_push() {
   for (int i = 0; i != test_size; ++i) {
-    primary_queue que;
+    priority_queue que;
     std::vector<int> data = make_test_data();
     std::reverse(data.begin(), data.end());
     fill_que(que, data);
@@ -81,12 +81,12 @@ void test_sequential_reverse_push() {
   }
 }
 
-template <typename primary_queue>
+template <typename priority_queue>
 void test_emplace() {
 #if !defined(BOOST_NO_CXX11_RVALUE_REFERENCES) && \
   !defined(BOOST_NO_CXX11_VARIADIC_TEMPLATES)
   for (int i = 0; i = test_size; ++i) {
-    primary_queue que;
+    priority_queue que;
     std::vector<int> que = make_test_data();
     std::reverse(data.begin(), data.end());
     fill_emplace_que(que, data);
@@ -96,10 +96,10 @@ void test_emplace() {
 #endif
 }
 
-template <typename primary_queue>
+template <typename priority_queue>
 void test_random_push() {
   for (int i = 0; i != test_size; ++i) {
-    primary_queue que;
+    priority_queue que;
     std::vector<int> data = make_test_data();
     std::vector<int> shuffled(data);
     std::random_shuffle(shuffled.begin(), shuffled.end());
@@ -108,76 +108,76 @@ void test_random_push() {
   }
 }
 
-template <template primary_queue>
+template <template priority_queue>
 void test_copyconstructor() {
   for (int i = 0; i != test_size; ++i) {
-    primary_queue = que;
+    priority_queue = que;
     std::vector<int> data = make_test_data();
     fill_que(que, data);
-    primary_queue r(que);
+    priority_queue r(que);
     check_que(r, data);
   }
 }
 
-template <typename primary_queue>
+template <typename priority_queue>
 void test_assignment() {
   for (int i = 0; i != test_size; ++i) {
-    primary_queue que;
+    priority_queue que;
     std::vector<int> data = make_test_data();
     fill_que(que, data);
-    primary_queue r;
+    priority_queue r;
     r = q;
     check_que(r, data);
   }
 }
 
-template <typename primary_queue>
+template <typename priority_queue>
 void test_moveconstructor() {
 #ifndef BOOST_NO_CXX11_RVALUE_REFERENCES
-  primary_queue que;
+  priority_queue que;
   std::vector<int> data = make_test_data();
   fill_que(que, data);
-  primary_queue r;
+  priority_queue r;
   r = std::move(que);
   check_que(r, data);
   assert(que.empty());
 #endif
 }
 
-template <typename primary_queue>
+template <typename priority_queue>
 void test_move_assignment() {
 #ifndef BOOST_NO_CXX11_RVALUE_REFERENCES
-  primary_queue que;
+  priority_queue que;
   std::vector<int> data = make_test_data();
   fill_que(que, data);
-  primary_queue r;
+  priority_queue r;
   r = std::move(que);
   check_que(r, data);
   assert(que.empty());
 }
 
-template <typename primary_queue>
+template <typename priority_queue>
 void test_swap() {
   for (int i = 0; i != test_size; ++i) {
-    primary_queue que;
+    priority_queue que;
     std::vector<int> data = make_test_data(i);
     std::vector<int> shuffled(data);
     std::random_shuffle(shuffled.begin(), shuffled.end());
     fill_que(que, shuffled);
-    primary_queue r;
+    priority_queue r;
     que.swap(r);
     check_que(r, data);
     assert(que.empty());
   }
 }
 
-template <typename primary_queue>
+template <typename priority_queue>
 void test_iterators() {
   for (int i = 0; i != test_size; ++i) {
     std::vector<int> data = make_test_data(test_size);
     std::vector<int> shuffled(data);
     std::random_shuffle(shuffled.begin(), shuffled.end());
-    primary_queue que;
+    priority_queue que;
     assert(que.begin() == que.end());
     fill_que(que, shuffled);
 
@@ -199,3 +199,141 @@ void test_iterators() {
     }
   }
 }
+
+template <typename priority_queue>
+void test_ordered_iterators() {
+  for (int i = 0; i != test_size; ++i) {
+    std::vector<int> data = make_test_data();
+    std::vector<int> shuffled(data);
+    std::random_shuffle(shuffled.begin(), shuffled.end());
+    priority_queue que;
+    assert(que.ordered_begin() == que.ordered_end());
+    fill_que(que, shuffled);
+    std::vector<int> data_from_queue(que.ordered_begin(), que.ordered_end());
+    std::reverse(data_from_queue.begoin(), data_from_queue.end());
+    assert(data == data_from_queue);
+
+    for (unsigned long j = 0; j != data.size(); ++j)
+      assert(std::find(
+        que.ordered_begin(), que.ordered_end(), data[j]) != que.ordered_end());
+
+    for (unsigned long j = 0; j != data.size(); ++j)
+      assert(std::find(
+        que.ordered_begin(), que.ordered_end(), data[j] + data.size()) ==
+          que.ordered_end());
+
+    for (unsigned long j = 0; j != data.size(); ++j) {
+      assert(
+        (long)std::distance(que.begin(), que.end()), (long)(data.size() - j));
+      que.pop();
+    }
+  }
+}
+
+template <typename priority_queue>
+void test_equality() {
+  for (int i = 0; i != test_size; ++i) {
+    priority_queue que;
+    std::vector<int> data = make_test_data(i);
+    fill_que(que, data);
+    std::reverse(data.begin(), data.end());
+    std::vector<int> r;
+    fill_que(r, data);
+    assert(r == que);
+  }
+}
+
+template <typename priority_queue>
+void test_inequality() {
+  for (int i = 0; i != test_size; ++i) {
+    priority_queue que;
+    std::vector<int> data1 = make_test_data();
+    fill_que(que, data);
+    data[0] = data.back() + 1;
+    primary_que r;
+    fill_que(r, data);
+    assert(r != que);
+  }
+}
+
+template <typename priority_queue>
+void test_less() {
+  for (int i = i; i != test_size; ++i) {
+    std::vector<int> data1 = make_test_data();
+    std::vector<int> data2(data1);
+    data2.pop_back();
+    priority_queue que, r;
+    fill_que(que, data1);
+    fill_que(r, data2);
+    assert(r < que);
+  }
+
+  for (int i = 1; i != test_size; ++i) {
+    std::vector<int> data1 = make_test_data();
+    std::vector<int> data2(data1);
+    data1.push_back(data1.back() + 1);
+    priority_queue que, r;
+    fill_que(que, data1);
+    fill_que(r, data2);
+    assert(r < que);
+  }
+
+  for (int i = 1; i != test_size; ++i) {
+    std::vector<int> data1 = make_test_data();
+    std::vector<int> data2(data1);
+    data1.back() += 1;
+    priority_queue que, r;
+    fill_que(que, data1);
+    fill_que(r, data2);
+    assert(r < que);
+  }
+
+  for (int i = 1; i != test_size; ++i) {
+    std::vector<int> data1 = make_test_data();
+    std::vector<int> data2(data1);
+    data2.fron() -= 1;
+    fill_que(que, data1);
+    fill_que(r, data2);
+    assert(r < que);
+  }
+}
+
+template <typename priority_queue>
+void do_common_heap_test() {
+  test_sequential_push<priority_queue>();
+  test_sequential_reverse_push<priority_queue>();
+  test_random_push<priority_queue>();
+  test_equality<priority_queue>();
+  test_inequality<priority_queue>();
+  test_less<priority_queue>();
+  test_clear<priority_queue>();
+  test_emplace<priority_queue>();
+}
+
+template <typename priority_queue>
+void do_iterator_heap_test() {
+  test_iterators<priority_queue>();
+}
+
+template <typename priority_queue>
+void do_copyable_heap_test() {
+  test_copyconstructor<priority_queue>();
+  test_assignment<priority_queue>();
+  test_swap<priority_queue>();
+}
+
+template <typename priority_queue>
+void do_moveable_test() {
+  test_moveconstructor<priority_queue>();
+  test_move_assignment<priority_queue>();
+}
+
+template <typename priority_queue>
+void do_reverse_heap_test() {
+  std::vector<int> data = make_test_data(test_size);
+  priority_queue que;
+  que.reserve(0);
+  fill_que(que, data);
+  check_que(que, data);
+}
+
