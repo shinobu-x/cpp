77fbb670d110ffa62baa13a72df92de573833dd0
Shinobu Kinjo <shinobu@redhat.com>
2017-09-23 10:02:51 +0900

    new file:   test/heap/common.hpp new file:   test/heap/merge.hpp new file:   test/heap/stable.hpp new file:   test/heap/test_priority_queue.cpp

---
+++ test/heap/test_priority_queue.cpp
@@ -0,0 +1,11 @@
+#include <boost/heap/priority_queue.hpp>
+
+#include <algorithm>
+
+#include "common.hpp"
+#include "stable.hpp"
+#include "merge.hpp"
+
+auto main() -> decltype(0) {
+  return 0;
+}
