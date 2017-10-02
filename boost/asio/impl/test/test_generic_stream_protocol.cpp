d01922703bd37e5fb6cfc08bd53e8486ce368666
Shinobu Kinjo <shinobu@redhat.com>
2017-10-03 08:25:24 +0900

    modified:   boost/asio/impl/test/test_generic_stream_protocol.cpp

--- boost/asio/impl/test/test_generic_stream_protocol.cpp
+++ boost/asio/impl/test/test_generic_stream_protocol.cpp
@@ -31,37 +31,41 @@ void test_1() {
     boost::asio::socket_base::bytes_readable io_control_command;
     boost::system::error_code ec;
 
-    boost::asio::stream_protocol::socket socket1(ios);
-    boost::asio::stream_protocol::socket socket2(ios,
-      boost::asio::stream_protocol(af_inet, ipproto_tcp));
-    boost::asio::stream_protocol::socket socket3(ios,
-      boost::asio::stream_protocol::endpoint());
+    boost::asio::generic::stream_protocol::socket socket1(ios);
+    boost::asio::generic::stream_protocol::socket socket2(ios,
+      boost::asio::generic::stream_protocol(af_inet, ipproto_tcp));
+    boost::asio::generic::stream_protocol::socket socket3(ios,
+      boost::asio::generic::stream_protocol::endpoint());
 
-    boost::asio::stream_protocol::socket::native_handle_type native_socket1 =
-      ::socket(af_inet, sock_stream, 0);
-    boost::asio::stream_protocol::socket socket4(ios,
-      boost::asio::stream_protocol(af_inet, ipproto_tcp), native_socket1);
+    boost::asio::generic::stream_protocol::socket::native_handle_type
+      native_socket1 = ::socket(af_inet, sock_stream, 0);
+    boost::asio::generic::stream_protocol::socket socket4(ios,
+      boost::asio::generic::stream_protocol(
+        af_inet, ipproto_tcp), native_socket1);
 
-    socket1 = boost::asio::stream_protocol::socket(ios);
+    socket1 = boost::asio::generic::stream_protocol::socket(ios);
     socket1 = std::move(socket2);
     socket1 = boost::asio::ip::tcp::socket(ios);
 
     boost::asio::io_service& ios_ref = socket1.get_io_service();
     (void)ios_ref;
 
-    boost::asio::stream_protocol::socket::lowest_layer_type& lowest_layer =
-      socket1.lowest_layer();
+    boost::asio::generic::stream_protocol::socket::lowest_layer_type&
+      lowest_layer = socket1.lowest_layer();
 
-    socket1.open(boost::asio::stream_protocol(af_inet, ipproto_tcp));
-    socket1.open(boost::asio::stream_protocol(af_inet, ipproto_tcp), ec);
+    socket1.open(boost::asio::generic::stream_protocol(
+      af_inet, ipproto_tcp));
+    socket1.open(boost::asio::generic::stream_protocol(
+      af_inet, ipproto_tcp), ec);
 
-    boost::asio::stream_protocol::socket::native_handle_type native_socket2 =
-      ::socket(af_inet, sock_stream, 0);
+    boost::asio::generic::stream_protocol::socket::native_handle_type
+      native_socket2 = ::socket(af_inet, sock_stream, 0);
     socket1.assign(
-      boost::asio::stream_protocol(af_inet, ipproto_tcp), native_socket2);
+      boost::asio::generic::stream_protocol(
+        af_inet, ipproto_tcp), native_socket2);
 
-    boost::asio::stream_protocol::socket::native_handle_type native_socket3 =
-      ::socket(af_inet, sock_stream, 0);
+    boost::asio::generic::stream_protocol::socket::native_handle_type
+      native_socket3 = ::socket(af_inet, sock_stream, 0);
 
     bool is_open = socket1.is_open();
     (void)is_open;
@@ -79,16 +83,16 @@ void test_1() {
     std::size_t available2 = socket1.available(ec);
     (void)available2;
 
-    socket1.bind(boost::asio::stream_protocol::endpoint());
-    socket1.bind(boost::asio::stream_protocol::endpoint(), ec);
+    socket1.bind(boost::asio::generic::stream_protocol::endpoint());
+    socket1.bind(boost::asio::generic::stream_protocol::endpoint(), ec);
 
-    boost::asio::stream_protocol::endpoint endpoint1 =
+    boost::asio::generic::stream_protocol::endpoint endpoint1 =
       socket1.local_endpoint();
-    boost::asio::stream_protocol::endpoint endpoint2 =
+    boost::asio::generic::stream_protocol::endpoint endpoint2 =
       socket1.local_endpoint(ec);
-    boost::asio::stream_protocol::endpoint endpoint3 =
+    boost::asio::generic::stream_protocol::endpoint endpoint3 =
       socket1.remote_endpoint();
-    boost::asio::stream_protocol::endpoint endpoint4 =
+    boost::asio::generic::stream_protocol::endpoint endpoint4 =
       socket1.remote_endpoint();
 
     socket1.shutdown(boost::asio::socket_base::shutdown_both);
@@ -120,12 +124,63 @@ void test_1() {
     socket1.receive(
       boost::asio::buffer(mutable_char_buffer));
     socket1.receive(
-      boost::asio::buufer(mutable_char_buffer), in_flags);
+      boost::asio::buffer(mutable_char_buffer), in_flags);
     socket1.receive(
-      boost::asio::buffer(mutable_char_buffer), in_flags, ec)
+      boost::asio::buffer(mutable_char_buffer), in_flags, ec);
     socket1.receive(
       boost::asio::null_buffers());
     socket1.receive(
       boost::asio::null_buffers(), in_flags);
     socket1.receive(
-      boost::asoi::null_buffers(), in_flags, ec);
+      boost::asio::null_buffers(), in_flags, ec);
+
+    socket1.async_receive(
+      boost::asio::buffer(mutable_char_buffer), receive_handler);
+    socket1.async_receive(
+      boost::asio::buffer(mutable_char_buffer), in_flags, receive_handler);
+    socket1.async_receive(
+      boost::asio::null_buffers(), receive_handler);
+    socket1.async_receive(
+      boost::asio::null_buffers(), in_flags, receive_handler);
+
+    socket1.write_some(
+      boost::asio::buffer(mutable_char_buffer));
+    socket1.write_some(
+      boost::asio::buffer(mutable_char_buffer), ec);
+    socket1.write_some(
+      boost::asio::buffer(const_char_buffer));
+    socket1.write_some(
+      boost::asio::buffer(const_char_buffer), ec);
+    socket1.write_some(
+      boost::asio::null_buffers());
+    socket1.write_some(
+      boost::asio::null_buffers(), ec);
+
+    socket1.async_write_some(
+      boost::asio::buffer(mutable_char_buffer), write_some_handler);
+    socket1.async_write_some(
+      boost::asio::buffer(const_char_buffer), write_some_handler);
+    socket1.async_write_some(
+      boost::asio::null_buffers(), write_some_handler);
+
+    socket1.read_some(
+      boost::asio::buffer(mutable_char_buffer));
+    socket1.read_some(
+      boost::asio::buffer(mutable_char_buffer), ec);
+    socket1.read_some(
+      boost::asio::null_buffers());
+    socket1.read_some(
+      boost::asio::null_buffers(), ec);
+
+    socket1.async_read_some(
+      boost::asio::buffer(mutable_char_buffer), read_some_handler);
+    socket1.async_read_some(
+      boost::asio::null_buffers(), read_some_handler);
+  } catch (...) {}
+}
+} // namespace
+
+auto main() -> decltype(0) {
+  test_1();
+  return 0;
+}
