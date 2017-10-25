563a40df72c5421dea3ab0e684eb9b2ebe8c43b7
Shinobu Kinjo <shinobu@redhat.com>
2017-10-25 10:56:54 +0900

    modified:   boost/asio/impl/test/test_read_at.cpp

--- boost/asio/impl/test/test_read_at.cpp
+++ boost/asio/impl/test/test_read_at.cpp
@@ -1474,6 +1474,40 @@ void test_9() {
   assert(bytes_transferred == 50);
   assert(sb.size() == 50);
   assert(s.check_buffers(0, sb.data(), 50));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(10);
+  sb.consume(sb.size());
+  bytes_transferred = boost::asio::read_at(s, 1234, sb,
+    boost::asio::transfer_at_least(42));
+  assert(bytes_transferred == 50);
+  assert(sb.size() == 50);
+  assert(s.check_buffers(1234, sb.data(), 50));
+
+  s.reset(read_data, sizeof(read_data));
+  sb.consume(sb.size());
+  bytes_transferred = boost::asio::read_at(s, 0, sb,
+    boost::asio::transfer_exactly(1));
+  assert(bytes_transferred == 1);
+  assert(sb.size() == 1);
+  assert(s.check_buffers(0, sb.data(), 1));
+
+  s.reset(read_data, sizeof(read_data));
+  sb.consume(sb.size());
+  bytes_transferred = boost::asio::read_at(s, 1234, sb,
+    boost::asio::transfer_exactly(1));
+  assert(bytes_transferred == 1);
+  assert(sb.size() == 1);
+  assert(s.check_buffers(1234, sb.data(), 1));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(1);
+  sb.consume(sb.size());
+  bytes_transferred = boost::asio::read_at(s, 0, sb,
+    boost::asio::transfer_exactly(1));
+  assert(bytes_transferred == 1);
+  assert(sb.size() == 1);
+  assert(s.check_buffers(0, sb.data(), 1));
 }
 
 auto main() -> decltype(0) {
