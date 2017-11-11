cac081a9fd1581e18d731f24c90ed4c03ccaf7cb
Shinobu Kinjo <shinobu@redhat.com>
2017-11-11 17:46:45 +0900

    modified:   boost/asio/impl/test/test_read_at.cpp

--- boost/asio/impl/test/test_read_at.cpp
+++ boost/asio/impl/test/test_read_at.cpp
@@ -2913,6 +2913,15 @@ void test_12() {
   assert(!ec);
   assert(bytes_transferred == 50);
   assert(s.check_buffers(1234, sb.data(), 50));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(1);
+  sb.consume(sb.size());
+  bytes_transferred = boost::asio::read_at(s, 0, sb,
+    boost::asio::transfer_at_least(48));
+  assert(!ec);
+  assert(bytes_transferred == 48);
+  assert(s.check_buffers(0, sb.data(), 48));
 }
 
 auto main() -> decltype(0) {
