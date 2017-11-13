07ebc2efec99ebc8e7721f5954e55c250fd204d1
Shinobu Kinjo <shinobu@redhat.com>
2017-11-13 16:16:23 +0900

    modified:   boost/asio/impl/test/test_read_at.cpp

--- boost/asio/impl/test/test_read_at.cpp
+++ boost/asio/impl/test/test_read_at.cpp
@@ -3049,6 +3049,21 @@ void test_12() {
   assert(!ec);
   assert(bytes_transferred == 10);
   assert(s.check_buffers(1234, sb.data(), 10));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(10);
+  sb.consume(sb.size());
+  ec = boost::system::error_code();
+  bytes_transferred = boost::asio::read_at(s, 0, sb,
+    boost::asio::transfer_exactly(10), ec);
+  assert(!ec);
+  assert(bytes_transferred == 10);
+  assert(s.check_buffers(0, sb.data(), 10));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(10);
+  sb.consume(sb.size());
+  bytes_trans
 }
 
 auto main() -> decltype(0) {
