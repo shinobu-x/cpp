604fc70f0fae657da4408d3f18eba82467ebd9de
Shinobu Kinjo <shinobu@redhat.com>
2017-11-14 03:36:44 +0900

    modified:   boost/asio/impl/test/test_read_at.cpp

--- boost/asio/impl/test/test_read_at.cpp
+++ boost/asio/impl/test/test_read_at.cpp
@@ -3063,6 +3063,27 @@ void test_12() {
   s.reset(read_data, sizeof(read_data));
   s.next_read_length(10);
   sb.consume(sb.size());
+  ec = boost::system::error_code();
+  bytes_transferred = boost::asio::read_at(s, 1234, sb,
+    boost::asio::transfer_exactly(10), ec);
+  assert(!ec);
+  assert(bytes_transferred == 10);
+  assert(s.check_buffers(1234, sb.data(), 10));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(50);
+  sb.consume(sb.size());
+  ec = boost::system::error_code();
+  bytes_transferred = boost::asio::read_at(s, 0, sb,
+    boost::asio::transfer_exactly(10), ec);
+  assert(!ec);
+  assert(bytes_transferred == 10);
+  assert(s.check_buffers(0, sb.data(), 10));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(50);
+  sb.consume(sb.size());
+  ec = boost::system::error_code();
   bytes_transferred = boost::asio::read_at(s, 1234, sb,
     boost::asio::transfer_exactly(10), ec);
   assert(!ec);
