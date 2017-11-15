ba6bcfdfe8aed6fbff4e6e1c935b5c4274135da0
Shinobu Kinjo <shinobu@redhat.com>
2017-11-16 05:16:25 +0900

    modified:   boost/asio/impl/test/test_read_at.cpp

--- boost/asio/impl/test/test_read_at.cpp
+++ boost/asio/impl/test/test_read_at.cpp
@@ -3161,7 +3161,62 @@ void test_12() {
   assert(s.check_buffers(1234, sb.data(), 10));
 
   s.reset(read_data, sizeof(read_data));
+  s.next_read_length(50);
+  sb.consume(sb.size());
+  ec = boost::system::error_code();
+  bytes_transferred = boost::asio::read_at(s, 0, sb,
+    old_style_transfer_all, ec);
+  assert(!ec);
+  assert(bytes_transferred == 10);
+  assert(s.check_buffers(0, sb.data(), 10));
 
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(50);
+  sb.consume(sb.size());
+  ec = boost::system::error_code();
+  bytes_transferred = boost::asio::read_at(s, 1234, sb,
+    old_style_transfer_all, ec);
+  assert(!ec);
+  assert(bytes_transferred == 10);
+  assert(s.check_buffers(1234, sb.data(), 10));
+
+  s.reset(read_data, sizeof(read_data));
+  sb.consume(sb.size());
+  ec = boost::system::error_code();
+  bytes_transferred = boost::asio::read_at(s, 0, sb,
+    short_transfer, ec);
+  assert(!ec);
+  assert(bytes_transferred == sizeof(read_data));
+  assert(s.check_buffers(0, sb.data(), sizeof(read_data)));
+
+  s.reset(read_data, sizeof(read_data));
+  sb.consume(sb.size());
+  ec = boost::system::error_code();
+  bytes_transferred = boost::asio::read_at(s, 1234, sb,
+    short_transfer, ec);
+  assert(!ec);
+  assert(bytes_transferred == sizeof(read_data));
+  assert(s.check_buffers(1234, sb.data(), sizeof(read_data)));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(1);
+  sb.consume(sb.size());
+  ec = boost::system::error_code();
+  bytes_transferred = boost::asio::read_at(s, 0, sb,
+    short_transfer, ec);
+  assert(!ec);
+  assert(bytes_transferred == 1);
+  assert(s.check_buffers(0, sb.data(), 1));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(1);
+  sb.consume(sb.size());
+  ec = boost::system::error_code();
+  bytes_transferred = boost::asio::read_at(s, 1234, sb,
+    short_transfer, ec);
+  assert(!ec);
+  assert(bytes_transferred == 1);
+  assert(s.check_buffers(1234, sb.data(), 1));
 }
 
 auto main() -> decltype(0) {
