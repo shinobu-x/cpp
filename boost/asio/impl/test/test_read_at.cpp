44c21fb1e18e39a53cebb63202796a077d06dc16
Shinobu Kinjo <shinobu@redhat.com>
2017-11-20 05:25:39 +0900

    modified:   boost/asio/impl/test/test_read_at.cpp

--- boost/asio/impl/test/test_read_at.cpp
+++ boost/asio/impl/test/test_read_at.cpp
@@ -3349,7 +3349,7 @@ void test_13() {
 
   s.reset(read_data, sizeof(read_data));
   s.next_read_length(1);
-  memset(read_buf, 0, sizeof(read_data));
+  memset(read_buf, 0, sizeof(read_buf));
   called = false;
   boost::asio::async_read_at(s, 0, buffers,
     boost::asio::transfer_at_least(1),
@@ -3358,6 +3358,40 @@ void test_13() {
   ios.run();
   assert(called);
   assert(s.check_buffers(0, buffers, sizeof(read_data)));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(1);
+  memset(read_buf, 0, sizeof(read_buf));
+  called = false;
+  boost::asio::async_read_at(s, 1234, buffers,
+    boost::asio::transfer_at_least(1),
+    boost::bind(async_read_handler, _1, _2, sizeof(read_data), &called));
+  ios.reset();
+  ios.run();
+  assert(called);
+  assert(s.check_buffers(1234, buffers, sizeof(read_data)));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(10);
+  memset(read_buf, 0, sizeof(read_buf));
+  boost::asio::async_read_at(s, 0, buffers,
+    boost::asio::transfer_at_least(10),
+    boost::bind(async_read_handler, _1, _2, sizeof(read_data), &called));
+  ios.reset();
+  ios.run();
+  assert(called);
+  assert(s.check_buffers(0, buffers, sizeof(read_data)));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(10);
+  memset(read_buf, 0, sizeof(read_buf));
+  boost::asio::async_read_at(s, 1234, buffers,
+    boost::asio::transfer_at_least(10),
+    boost::bind(async_read_handler, _1, _2, sizeof(read_data), &called));
+  ios.reset();
+  ios.run();
+  assert(called);
+  assert(s.check_buffers(1234, buffers, sizeof(read_data)));
 }
 
 auto main() -> decltype(0) {
