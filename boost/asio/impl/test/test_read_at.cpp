fbdbcf6cbad72ddc7fd0b571dcfabb22eb5b74ab
Shinobu Kinjo <shinobu@redhat.com>
2017-11-24 16:48:36 +0900

    modified:   boost/asio/impl/test/test_read_at.cpp

--- boost/asio/impl/test/test_read_at.cpp
+++ boost/asio/impl/test/test_read_at.cpp
@@ -3488,6 +3488,18 @@ void test_13() {
   ios.run();
   assert(called);
   assert(s.check_buffers(1234, buffers, 1));
+
+  s.reset(read_data, sizeof(read_data));
+  s.next_read_length(10);
+  memset(read_buf, 0, sizeof(read_buf));
+  called = false;
+  boost::asio::async_read_at(s, 0, buffers,
+    boost::asio::transfer_exactly(1),
+    boost::bind(async_read_handler, _1, _2, 1, &called));
+  ios.reset();
+  ios.run();
+  assert(called);
+  assert(s.check_buffers(0, buffers, 1));
 }
 
 auto main() -> decltype(0) {
