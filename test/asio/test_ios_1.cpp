0c0ac4638dee729e4e5d2ce3c2d8d7741c21ec8c
Shinobu Kinjo <shinobu@redhat.com>
2018-01-17 10:00:03 +0900

    modified:   test/asio/test_ios_1.cpp

--- test/asio/test_ios_1.cpp
+++ test/asio/test_ios_1.cpp
@@ -54,6 +54,20 @@ void test_3() {
     i.join();
 }
 
+void test_4() {
+  std::cout << __func__ << '\n';
+  boost::asio::io_service ios;
+  std::vector<std::thread> v;
+  int n = 0;
+  for (int i = 0; i < 5; ++i) {
+    ios.post([&ios, &n](){std::cout << ++n << '\n';});
+    v.emplace_back([&ios]{ios.run();});
+  }
+  auto it = v.begin();
+  for (; it != v.end(); ++it)
+    it->join();
+}
+
 auto main() -> decltype(0) {
   test_1(); test_2(); test_3();
   return 0;
