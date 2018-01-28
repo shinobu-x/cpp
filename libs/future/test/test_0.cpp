eb476550fe7cbd995e462194600664b6cf1bd1d1
Shinobu Kinjo <shinobu@redhat.com>
2018-01-28 14:49:42 +0900

    modified:   libs/future/test/test_0.cpp

--- libs/future/test/test_0.cpp
+++ libs/future/test/test_0.cpp
@@ -17,6 +17,19 @@ struct test_callback {
 
 void doit() {
   {
+    boost::executor_ptr_type ex;
+    boost::mutex mutex;
+    boost::lock_guard<boost::mutex> lock_guard(mutex);
+    boost::unique_lock<boost::mutex> unique_lock;
+
+    boost::detail::shared_state_base base_type;
+    auto r = base_type.get_executor();
+    (void)r;
+    base_type.set_executor_policy(ex);
+//    base_type.set_executor_policy(ex, lock_guard);
+    base_type.set_executor_policy(ex, unique_lock);
+  }
+  {
     boost::promise<int> p;
     auto f = p.get_future();
     int i = 1;
