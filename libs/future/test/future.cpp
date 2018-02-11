3843ae0e7d0473ca7185e192b81d55fd17c83dd9
Shinobu Kinjo <shinobu@redhat.com>
2018-02-11 13:26:22 +0900

    modified:   libs/future/test/future.cpp

--- libs/future/test/future.cpp
+++ libs/future/test/future.cpp
@@ -1,6 +1,15 @@
 #include "../include/futures.hpp"
 #include "../hpp/future.hpp"
 
+void doit() {
+  {
+    boost::BOOST_THREAD_FUTURE<int> f1;
+    boost::BOOST_THREAD_FUTURE<boost::BOOST_THREAD_FUTURE<int> > f2;
+    boost::future<int> f3;
+    boost::future<boost::future<int> > f4;
+  }
+}
+
 auto main() -> decltype(0) {
   return 0;
 }
