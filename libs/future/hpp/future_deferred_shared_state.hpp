b88cf508813e4d93454e14a3585fb3d76dd6d402
Shinobu Kinjo <shinobu@redhat.com>
2018-02-18 16:06:01 +0900

    modified:   libs/future/hpp/future_deferred_shared_state.hpp

--- libs/future/hpp/future_deferred_shared_state.hpp
+++ libs/future/hpp/future_deferred_shared_state.hpp
@@ -1,6 +1,6 @@
 #ifndef FUTURE_DEFERRED_SHARED_STATE_HPP
 #define FUTURE_DEFERRED_SHARED_STATE_HPP
 
-#include "../impl/future_deferred_shared_state.ipp"
+#include <impl/future_deferred_shared_state.ipp>
 
 #endif // FUTURE_DEFERRED_SHARED_STATE_HPP
