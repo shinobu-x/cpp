e37feaaca549c17d94a74eb6f42f8ce83e75041c
Shinobu Kinjo <shinobu@redhat.com>
2018-02-23 08:44:52 +0900

    modified:   test_classify_image.py

--- deep_learning/tensorflow/test_classify_image.py
+++ deep_learning/tensorflow/test_classify_image.py
@@ -87,3 +87,95 @@ class NodeLookup(object):
         if node_id not in self.node_lookup
             return ''
         return self.node_lookup[node_id]
+
+def create_graph():
+    with tf.gfile.FastGFile(
+        os.path.join(
+            FLAGS.model_dir,
+            'classify_image_graph_def.pb'),
+        'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name = '')
+
+def run_inference_no_image(image):
+    if not tf.gfile.Exists(image):
+        tf.logging.fatal(
+            'File does not exist %s', image)
+    image_data = tf.gfile.FastGFile(image, 'rb').read()
+
+    create_graph()
+
+    with tf.Session() as sess:
+        softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+        predictions = sess.run(
+            softmax_tensor,
+            {'DecodeJpeg/contents:0' : image_data})
+
+        node_lookup = NodeLookup()
+
+        top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
+        for node_id in top_k:
+            human_string = node_lookup.id_to_string(node_id)
+            score = predictions[node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+
+def maybe_download_and_extract():
+    dest_directory = FLAG.model_dir
+
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+
+    filename = DATA_URL.split('/')[-1]
+    filepath = os.path.join(dest_directory, filename)
+
+    if not os.path.exists(filepath):
+        def _progress(count, block_size, total_size):
+            sys.stdout.write(
+                '\r>> Downloading %s %.1f%%' % (
+                    filename,
+                    float(count * block_size) / float(total_size) * 100.0))
+            sys.stdout.flush()
+
+        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+    tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+def main(_):
+    maybe_download_and_extract()
+    image = (
+        FLAGS.image_file if FLAGS.image_file else os.path.join(FLAGS.model_dir,
+        'cropped_panda.jpg'))
+    run_inference_on_image(image)
+
+if __name__ == '__main__'
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--model_dir',
+        type = str,
+        default = '/tmp'
+        help = """\
+        Path to classify_image_graph_def.pb,
+        imagenet_synset_to_human_label_map.txt, and
+        imagenet_2012_challenge_label_map_proto.pbtxt.\
+        """
+    )
+
+    parser.add_argument(
+        '--image_file',
+        type = str,
+        default = '',
+        help = 'Absolete path to image file.'
+    )
+
+    parser.add_argument(
+        '--num_top_prediction',
+        type = int,
+        default = 5,
+        help = 'Display this many predictions.'
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main = main, arg = [sys.argv[0]] + unparsed)
