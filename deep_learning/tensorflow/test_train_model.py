8ad909ed809ad19336d4aef31499a38d8623bf7a
Shinobu Kinjo <shinobu@redhat.com>
2018-02-24 17:55:18 +0900

    modified:   deep_learning/tensorflow/test_train_model.py

--- deep_learning/tensorflow/test_train_model.py
+++ deep_learning/tensorflow/test_train_model.py
@@ -262,3 +262,100 @@ def main(unused_args):
     eval_spec)
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser(
+    "type",
+    "bool",
+    lambda v: v.lower() == "true")
+
+  parser.add_argument(
+    "--training_data",
+    type = str,
+    default = "",
+    help = "Path to training data (tf.Example in TFRecord format).")
+
+  parser.add_argument(
+    "--eval_data",
+    type = str,
+    default = "",
+    help = "Path to evaluation data (tf.Example in TFRecord format).")
+
+  parser.add_argument(
+    "--classes_file",
+    type = str,
+    default = "",
+    help = "Path to a file with the classes - one class per line.")
+
+  parser.add_argument(
+    "--num_layers",
+    type = int,
+    default = 3,
+    help = "Number of recurrent nural network layers.")
+
+  parser.add_argument(
+    "--num_nodes",
+    type = int,
+    default = 128,
+    help = "Number of node per recurrent network layer.")
+
+  parser.add_argument(
+    "--conv_len",
+    type = str,
+    default = "[5, 5, 3]",
+    help = "Length of the convolution filters.")
+
+  parser.add_argument(
+    "--cell_type",
+    type = str,
+    default = "lstm",
+    help = "Cell type used for rnn layers: cudnn_lstm, lstm or block_lstm.")
+
+  parser.add_argument(
+    "--batch_norm",
+    type = "bool",
+    default = "False",
+    help = "Whether to enable batch normalization or not.")
+
+  parser.add_argument(
+    "--learning_rate",
+    type = float,
+    default = 0.0001,
+    help = "Learning rate used for training.")
+
+  parser.add_argument(
+    "--gradient_clipping_norm",
+    type = float,
+    default = 9.0,
+    help = "Gradient clipping norm used during training.")
+
+  parser.add_argument(
+    "--dropout",
+    type = float,
+    default = 0.3,
+    help = "Dropout used for convolutions and bidi lstm layers.")
+
+  parser.add_argument(
+    "--steps",
+    type = int,
+    default = 100000,
+    help = "Number of training steps.")
+
+  parser.add_argument(
+    "--batch_size",
+    type = int,
+    default = 8,
+    help = "Batch size to use for training and evaluations.")
+
+  parser.add_argument(
+    "--model_dir",
+    type = str,
+    default = "",
+    help = "path to store the model checkpoints.")
+
+  parser.add_argument(
+    "--self_test",
+    type = "bool",
+    help = "Whther to enable batch normalization or not.")
+
+  FLAGS, unparsed = parser.parser_known_args()
+  tf.app.run(main = main, argv = [sys.argv[0]] + unparsed)
+
