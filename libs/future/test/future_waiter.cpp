ed8c751cf330a3c5ee1b12147a36575c266376cb
Shinobu Kinjo <shinobu@redhat.com>
2018-02-26 08:47:53 +0900

    modified:   libs/future/test/future_waiter.cpp

--- libs/future/test/future_waiter.cpp
+++ libs/future/test/future_waiter.cpp
@@ -1,6 +1,13 @@
-#include "../include/futures.hpp"
-#include "../hpp/future_waiter.hpp"
+#include <include/futures.hpp>
+#include <hpp/future_waiter.hpp>
 
+void doit() {
+  {
+    boost::detail::future_waiter fw;
+    boost::future<int> f;
+    fw.add(f);
+  }
+}
 auto main() -> decltype(0) {
   return 0;
 }
