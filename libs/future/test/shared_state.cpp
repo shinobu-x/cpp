303223f6781434f9f18f86ad3d4c51e3e08232f1
Shinobu Kinjo <shinobu@redhat.com>
2018-02-27 08:30:34 +0900

    modified:   libs/future/test/shared_state.cpp

--- libs/future/test/shared_state.cpp
+++ libs/future/test/shared_state.cpp
@@ -1,6 +1,14 @@
-#include "../include/futures.hpp"
-#include "../hpp/shared_state.hpp"
+#include <include/futures.hpp>
+#include <hpp/shared_state.hpp>
+
+void doit() {
+  {
+    boost::detail::shared_state<int> s1;
+    boost::detail::shared_state<void> s2;
+  }
+}
 
 auto main() -> decltype(0) {
+  doit();
   return 0;
 }
