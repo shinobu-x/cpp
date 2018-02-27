3b409af79cad76e8565114afe017b900bfdae931
Shinobu Kinjo <shinobu@redhat.com>
2018-02-27 13:57:14 +0900

    modified:   libs/future/test/test_callback_1.cpp

--- libs/future/test/test_callback_1.cpp
+++ libs/future/test/test_callback_1.cpp
@@ -1,8 +1,9 @@
 #include <include/futures.hpp>
 
+template <typename T>
 struct Callback {
-  typedef boost::future<void> result_type;
-  typedef boost::future<void> future_type;
+  typedef boost::future<T> result_type;
+  typedef boost::future<T> future_type;
 
   result_type operator()(future_type future) const {
     assert(future.is_ready());
@@ -16,7 +17,7 @@ struct Callback {
     return boost::make_ready_future();
   }
 
-  result_type operator()(boost::shared_future<void> future) const {
+  result_type operator()(boost::shared_future<T> future) const {
     assert(future.is_ready());
     future.wait();
     return boost::make_ready_future();
@@ -31,7 +32,7 @@ struct Callback {
 
 void doit() {
   {
-    auto f = boost::make_ready_future().then(Callback());
+    auto f = boost::make_ready_future().then(Callback<void>());
     static_assert(
       std::is_same<
         decltype(f),
@@ -40,7 +41,7 @@ void doit() {
     f.wait();
   }
   {
-    auto f1 = boost::make_ready_future().then(Callback());
+    auto f1 = boost::make_ready_future().then(Callback<void>());
     static_assert(
       std::is_same<
         decltype(f1),
@@ -55,7 +56,7 @@ void doit() {
     f2.wait();
   }
   {
-    auto f1 = boost::make_ready_future().then(Callback());
+    auto f1 = boost::make_ready_future().then(Callback<void>());
     static_assert(
       std::is_same<
         decltype(f1),
@@ -67,6 +68,39 @@ void doit() {
         decltype(f2),
         boost::future<void> >::value
     );
+  }
+  {
+    auto f1 = boost::make_ready_future().then(Callback<void>());
+    static_assert(
+      std::is_same<
+        decltype(f1),
+        boost::future<boost::future<void> > >::value
+    );
+    auto f2 = f1.unwrap();
+    static_assert(
+      std::is_same<
+        decltype(f2),
+        boost::future<void> >::value
+    );
+    auto f3 = f2.then(Callback<void>());
+    static_assert(
+      std::is_same<
+        decltype(f3),
+        boost::future<boost::future<void> > >::value
+    );
+    f3.wait();
+  }
+  {
+    auto f = boost::make_ready_future().
+    then(Callback<void>()).
+    unwrap().
+    then(Callback<void>()).get();
+    static_assert(
+      std::is_same<
+        decltype(f),
+        boost::future<void> >::value
+    );
+    f.wait();
   }
 }
 
