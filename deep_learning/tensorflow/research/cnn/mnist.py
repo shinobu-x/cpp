c93a523f86a5cdf3b7487de6b0b15d590a6cda44
Shinobu Kinjo <shinobu@redhat.com>
2018-02-27 11:49:11 +0900

    modified:   deep_learning/tensorflow/research/cnn/mnist.py

--- deep_learning/tensorflow/research/cnn/mnist.py
+++ deep_learning/tensorflow/research/cnn/mnist.py
@@ -170,4 +170,138 @@ def cnn_model_fn(feature, labels, mode):
     inputs = dropout,
     units = 10)
 
+  # Generate predictions
+  # The logits layer of our model returns our predictions as raw values in a [
+  # batch_size, 10] - demensioanl tensor. Let's convert these raw values into
+  # two different formats that our model function can return.
+  #
+  # The predicted class for each example: A digit from 0 - 9.
+  # The probabilities for each possible target class for each example: The pro-
+  # bability that the example is a 0, is a 1, is a 2, etc.
+  predictions = {
+    # Generate predictions (for PREDICT and EVAL mode)
+    "classes": tf.argmax(
+      input = logits,
+      exis = 1),
+    # Adds softmax_layer to the graph. It is used for PREDICT and by the loggi-
+    # ng_hook
+    "probabilities": tf.nn.softmax(
+      logits,
+      name = "softmax_tensor")
+  }
+  # Compiles our prediction in a dict, and returns an EstimatorSpec object.
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(
+      mode = mode,
+      predictions = prediction)
 
+  # Calculates loss
+  # For both training and evaluation, we need to define a loss function that m-
+  # easures how closely the model's predictions match the target classes. For
+  # multiclass classification problems like MNIST, cross entropy is typically
+  # used as the loss metric.
+  loss = tf.losses.sparse_softmax_cross_entropy(
+    labels = labels,
+    logits = logits)
+
+  # Configures the Training Op
+  # In the previous section, we defined loss for our CNN as the softmax cross
+  # entrooy of the logts layer and our labels. Let's configure our model to op-
+  # timize this loss value during training. We will use a learning rate of 0.00
+  # 1 and stochastic gradient descent as the optimization algorithm.
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    optimizer = tf.train.GradientDescentOptimizer(
+      learning_rate = 0.001)
+    train_op = optimizer.minimize(
+      loss = loss,
+      global_step = tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(
+      mode = mode,
+      loss = loss,
+      train_op = train_op)
+
+  # Adds evalution metrics
+  eval_metrics_ops = {
+    "accuracy": tf.metrics.accuracy(
+      labels = labels,
+      predictions = predictions["classes"])}
+  return tf.estimator.EstimatorSpec(
+    mode = mode,
+    loss = loss,
+    eval_metrics_ops = eval_metrics_ops)
+
+def main(unused_argv):
+  # Loads training and eval data
+  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+  train_data = mnist.train.images # Returns np.array
+  train_labels = np.asarray(
+    mnist.train.labels,
+    dtype = np.int32)
+  eval_data = mnist.test.images # Returns np.array
+  eval_labels  np.asarray(
+    mnist.test.labels,
+    dtype = np.int32)
+
+  # Creates the estimator
+  # The model_fn argument specifies the model function to use for training, ev-
+  # aluation. We pass the cnn_mode_fn. The model_dir argument specifies the di-
+  # rectory where model data (checkpoints) will be saved.
+  mnist_classifier = tf.estimator.Estimator(
+    model_fn = cnn_model_fn,
+    model_dir = "/tmp/mnist_convert_mode")
+
+  # Sets up a logging hook
+  # Since CNNs can take a while to train, let's set up some logging so we can
+  # track progress during training. We can use tf.train.SessionRunHook to crea-
+  # te a tf.train.Logging.TensorHook that will log the probability values from
+  # the softmax layer of our CNN.
+  #
+  # We create the LoggingTensorHook, passing tensors_to_log to the tensors arg-
+  # ument. We set every_n_iter = 50, which specifies that probabilities should
+  # be logged every 50 steps of training.
+  tensors_to_log = {"probabilities": "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(
+    tensors = tensors_to_log,
+    every_n_iter = 50)
+
+  # Trains the model by creating train_input_fn and calling train() on mnist_c-
+  # lassifier.
+  #
+  # In the numpy_input_fn call, we pass the training feature data and labels to
+  # x (as a dict) and y, respectively. We set a batch_size of 100 (which means
+  # that the model will train on minibatches of 100 examples at each step).
+  # num_epochs = None means that the model will train until the specified numb-
+  # er of steps is reached. We also set shuffle = True to shuffle the training
+  # data. In the train call, we set steps = 20000 (which means the model will
+  # train for 20000 steps total). We pass our logging_hook to the hooks argume-
+  # nt, so that it will be triggered during training.
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x = {"x": train_data,
+    y = train_labels,
+    batch_size = 100,
+    num_epochs = None,
+    shuffle = True)
+  mnist_classifier.train(
+    input_fn = train_input_fn,
+    steps = 20000,
+    hooks = [logging_hook])
+
+  # Evaluates the model
+  # Once training is complete, we want to evaluate our model to determine its
+  # accuracy on the MNIST test set. We call the evaluate method, which evaluat-
+  # es the metrics we specified in eval_metric_ops argument in the model_fn.
+  #
+  # To create eval_input_fn, we set num_epochs = 1, so that the model evaluates
+  # the metrics over one epoch of data and returns the result. We also set shu-
+  # ffle = False to iterate through the data sequentially.
+  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x = {"x": eval_data},
+    y = eval_labels,
+    num_epochs = 1,
+    shuffle = False)
+  eval_results = mnist_classifier.evaluate(
+    input_fn = eval_input_fn)
+  print(eval_result)
+
+if __name__ == "__main__":
+  tf.app.run()
