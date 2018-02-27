c7059f7b7cd5d2753d8a0855187207f57f2841e0
Shinobu Kinjo <shinobu@redhat.com>
2018-02-28 07:49:32 +0900

    modified:   libs/future/test/test_callback_2.cpp

--- libs/future/test/test_callback_2.cpp
+++ libs/future/test/test_callback_2.cpp
@@ -7,24 +7,28 @@ struct Callback {
   typedef boost::future<T> future_type;
 
   result_type operator()(future_type future) const {
+    std::cout << __LINE__ << '\n';
     assert(future.is_ready());
     future.wait();
     return boost::make_ready_future();
   }
 
   result_type operator()(boost::future<future_type> future) const {
+    std::cout << __LINE__ << '\n';
     assert(future.is_ready());
     assert(future.get().is_ready());
     return boost::make_ready_future();
   }
 
   result_type operator()(boost::shared_future<T> future) const {
+    std::cout << __LINE__ << '\n';
     assert(future.is_ready());
     future.wait();
     return boost::make_ready_future();
   }
 
   result_type operator()(boost::shared_future<future_type> future) const {
+    std::cout << __LINE__ << '\n';
     assert(future.is_ready());
     assert(future.get().is_ready());
     return boost::make_ready_future();
@@ -47,6 +51,30 @@ void doit() {
         boost::future<boost::future<void> > >::value
     );
     f2.wait();
+  }
+  {
+    boost::basic_thread_pool executor(2);
+    auto f1 = boost::make_ready_future().then(executor, Callback<void>());
+    static_assert(
+      std::is_same<
+        decltype(f1),
+        boost::future<boost::future<void> > >::value
+    );
+    std::cout << (int)f1.valid() << '\n';
+    auto f2 = f1.unwrap();
+    static_assert(
+      std::is_same<
+        decltype(f2),
+        boost::future<void> >::value
+    );
+    std::cout << (int)f2.valid() << '\n';
+    auto f3 = f2.then(executor, Callback<void>());
+    static_assert(
+      std::is_same<
+        decltype(f3),
+        boost::future<boost::future<void> > >::value
+    );
+    f3.wait();
   }
 }
 
