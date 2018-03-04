39ca0e136b4cb8469329cedb05aaeefd636aa8a9
Shinobu Kinjo <shinobu@redhat.com>
2018-03-04 12:31:50 +0900

    modified:   libs/future/test/packaged_task.cpp

--- libs/future/test/packaged_task.cpp
+++ libs/future/test/packaged_task.cpp
@@ -1,9 +1,31 @@
 #include <include/futures.hpp>
 #include <hpp/packaged_task.hpp>
 
+boost::condition_variable cv;
+boost::mutex m;
+int ans = 0;
+bool is_ready = false;
+
 int a() {
   std::cout << __func__ << '\n';
   return 1;
+}
+
+void b() {
+  std::cout << __func__ << '\n';
+  boost::unique_lock<boost::mutex> lock(m);
+  boost::this_thread::sleep_for(boost::chrono::seconds(5));
+  ans = 1;
+  is_ready = true;
+}
+
+void c() {
+  std::cout << __func__ << '\n';
+  boost::unique_lock<boost::mutex> lock(m);
+  while (!is_ready) {
+    cv.wait(lock);
+  }
+  ans = 2;
 }
 
 void invoke_lazy_task(boost::packaged_task<int()>& task) {
@@ -14,14 +36,20 @@ void invoke_lazy_task(boost::packaged_task<int()>& task) {
 }
 
 void doit() {
-  boost::packaged_task<int()> task(a);
-  task.set_wait_callback(invoke_lazy_task);
-  auto f(task.get_future());
-  assert(!f.is_ready());
-  assert(!f.has_value());
-  auto r = f.get();
-  assert(f.is_ready());
-  assert(f.has_value());
+  {
+    boost::packaged_task<int()> task(a);
+    task.set_wait_callback(invoke_lazy_task);
+     auto f(task.get_future());
+    assert(!f.is_ready());
+    assert(!f.has_value());
+    auto r = f.get();
+    assert(f.is_ready());
+    assert(f.has_value());
+  }
+  {
+    b();
+    c();
+  }
 }
 
 auto main() -> decltype(0) {
