35a4c71d85fa7f2c3595bb8acdfce5f1e21e0987
Shinobu Kinjo <shinobu@redhat.com>
2018-03-04 12:11:03 +0900

    modified:   libs/future/test/packaged_task.cpp

--- libs/future/test/packaged_task.cpp
+++ libs/future/test/packaged_task.cpp
@@ -2,10 +2,12 @@
 #include <hpp/packaged_task.hpp>
 
 int a() {
+  std::cout << __func__ << '\n';
   return 1;
 }
 
 void invoke_lazy_task(boost::packaged_task<int()>& task) {
+  std::cout << __func__ << '\n';
   try {
     task();
   } catch (...) {}
@@ -14,6 +16,12 @@ void invoke_lazy_task(boost::packaged_task<int()>& task) {
 void doit() {
   boost::packaged_task<int()> task(a);
   task.set_wait_callback(invoke_lazy_task);
+  auto f(task.get_future());
+  assert(!f.is_ready());
+  assert(!f.has_value());
+  auto r = f.get();
+  assert(f.is_ready());
+  assert(f.has_value());
 }
 
 auto main() -> decltype(0) {
