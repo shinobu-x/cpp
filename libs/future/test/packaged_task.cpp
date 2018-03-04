906ec24147be0baf978c500fae4653ee1e507f5e
Shinobu Kinjo <shinobu@redhat.com>
2018-03-04 11:52:56 +0900

    modified:   libs/future/test/packaged_task.cpp

--- libs/future/test/packaged_task.cpp
+++ libs/future/test/packaged_task.cpp
@@ -1,6 +1,22 @@
-#include "../include/futures.hpp"
-#include "../hpp/packaged_task.hpp"
+#include <include/futures.hpp>
+#include <hpp/packaged_task.hpp>
+
+int a() {
+  return 1;
+}
+
+void invoke_lazy_task(boost::packaged_task<int()>& task) {
+  try {
+    task();
+  } catch (...) {}
+}
+
+void doit() {
+  boost::packaged_task<int()> task(a);
+  task.set_wait_callback(invoke_lazy_task);
+}
 
 auto main() -> decltype(0) {
+  doit();
   return 0;
 }
