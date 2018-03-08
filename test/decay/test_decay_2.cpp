effa7523e756145e30cfc9cc58487b41ecee2422
Shinobu Kinjo <shinobu@redhat.com>
2018-03-09 07:49:32 +0900

    modified:   test/decay/test_decay_2.cpp

--- test/decay/test_decay_2.cpp
+++ test/decay/test_decay_2.cpp
@@ -1,5 +1,19 @@
-#include <boost/type_traits/decay.hpp>
+#include <boost/type_traits.hpp>
+
+void doit() {
+  static_assert(boost::is_same<
+    boost::decay<int>::type, int>::value);
+  static_assert(boost::is_same<
+    boost::decay<char[2]>::type, char*>::value);
+  static_assert(boost::is_same<
+    boost::decay<char[2][3]>::type, char(*)[3]>::value);
+  static_assert(boost::is_same<
+    boost::decay<const char[2]>::type, const char*>::value);
+  static_assert(boost::is_same<
+    boost::decay<wchar_t[2]>::type, wchar_t*>::value);
+}
 
 auto main() -> decltype(0) {
+  doit();
   return 0;
 }
