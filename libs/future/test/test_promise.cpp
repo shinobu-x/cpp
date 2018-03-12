2c88b287e57eac64a294e46d4d8bf53dffc1a8c2
Shinobu Kinjo <shinobu@redhat.com>
2018-03-13 05:14:51 +0900

    modified:   libs/future/test/test_promise.cpp

--- libs/future/test/test_promise.cpp
+++ libs/future/test/test_promise.cpp
@@ -12,6 +12,28 @@ void doit() {
     }
     assert(thrown);
   }
+  {
+    boost::promise<int&> p1;
+    boost::future<int&> f1 = p1.get_future();
+    bool thrown = false;
+    try {
+      f1 = p1.get_future();
+    } catch (boost::future_already_retrieved const&) {
+      thrown = true;
+    }
+    assert(thrown);
+  }
+  {
+    boost::promise<void> p1;
+    boost::future<void> f1 = p1.get_future();
+    bool thrown = false;
+    try {
+      f1 = p1.get_future();
+    } catch (boost::future_already_retrieved const&) {
+      thrown = true;
+    }
+    assert(thrown);
+  }
 }
 
 auto main() -> decltype(0) {
