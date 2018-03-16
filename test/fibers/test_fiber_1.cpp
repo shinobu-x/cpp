8c629fbb4b0b179d55867e66f500e77aaac4aedf
Shinobu Kinjo <shinobu@redhat.com>
2018-03-16 12:39:16 +0900

    new file:   test/fibers/test_fiber_1.cpp

---
+++ test/fibers/test_fiber_1.cpp
@@ -0,0 +1,72 @@
+#include <boost/fiber/all.hpp>
+#include <boost/throw_exception.hpp>
+#include <stdexcept>
+
+int v = 1;
+
+struct xp : public std::runtime_error {
+  xp() : std::runtime_error("Opps") {}
+};
+
+struct data {
+  data() = default;
+  data(data const&) = delete;
+  data(data&&) = default;
+  data& operator=(data const&) = delete;
+  data& operator=(data&&) = default;
+  int value = 0;
+};
+
+void f1(boost::fibers::promise<int>* p, int i) {
+  boost::this_fiber::yield();
+  p->set_value(i);
+}
+
+void f2() {
+  boost::fibers::promise<int> p;
+  boost::fibers::future<int> f = p.get_future();
+  assert(f.valid());
+  boost::this_fiber::yield();
+  boost::fibers::fiber(boost::fibers::launch::dispatch, f1, &p, 2).detach();
+  boost::this_fiber::yield();
+  assert(2 == f.get());
+}
+
+int f3() {
+  return 3;
+}
+
+void f4() {}
+
+int f5() {
+  boost::throw_exception(xp());
+  return 5;
+}
+
+void f6() {
+  boost::throw_exception(xp());
+}
+
+int& f7() {
+  return v;
+}
+
+int f8(int v) {
+  return v;
+}
+
+data f9() {
+  data d;
+  d.value = 9;
+  return d;
+}
+
+data f10() {
+  data d;
+  boost::throw_exception(xp());
+  return d;
+}
+
+auto main() -> decltype(0) {
+  return 0;
+}
