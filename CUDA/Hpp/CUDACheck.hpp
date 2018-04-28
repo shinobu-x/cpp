cc570959ab171bbae4f334e6226765b482ac342b
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-04-29 08:56:27 +0900

    new file:   CUDA/Hpp/CUDACheck.hpp

---
+++ CUDA/Hpp/CUDACheck.hpp
@@ -0,0 +1,15 @@
+#ifndef CUDACHECK_HPP
+#define CUDACHECK_HPP
+
+#include <cuda_runtime.h>
+#include <sstream>
+
+inline void cuda_check(const char* expr, cudaError_t status, const char* file,
+  int line) {
+  if (status != cudaSuccess) {
+    std::stringstream error;
+    error << cudaGetErrorString(status) << "\n\t" << expr << " at " << file
+      << '.' << line << std::endl;
+  }
+}
+#endif // CUDACHECK_HPP
