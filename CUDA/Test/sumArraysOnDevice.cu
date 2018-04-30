02f8f390fb93d4dd829ae80a8af7a7e9a1639b2f
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-04-30 19:29:43 +0900

    modified:   CUDA/Test/sumArraysOnDevice.cu

--- CUDA/Test/sumArraysOnDevice.cu
+++ CUDA/Test/sumArraysOnDevice.cu
@@ -1,7 +1,10 @@
 #include <cstdlib>
+#include <cuda_runtime.h>
 
-#include "../HPP/sumArraysOnDevice.hpp"
+#include "../HPP/InitData.hpp"
 #include "../HPP/cudaSetupDevice.hpp"
+#include "../HPP/sumArraysOnDevice.hpp"
+
 
 auto main() -> decltype(0) {
   cudaSetupDevice();
@@ -18,6 +21,10 @@ auto main() -> decltype(0) {
   h_b = (value_type*)malloc(nbytes);
   h_c = (value_type*)malloc(nbytes);
 
+  // Data initializations
+  InitData(h_a, threads);
+  InitData(h_b, threads);
+
   // Addresses reservations for Device
   value_type* d_a;
   value_type* d_b;
@@ -26,7 +33,18 @@ auto main() -> decltype(0) {
   cudaMalloc((value_type**)&d_b, nbytes);
   cudaMalloc((value_type**)&d_c, nbytes);
 
-  // Copy  
-    
+  // Copy data from host to device  
+  cudaMemcpy(d_a, h_a, nbytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, h_b, nbytes, cudaMemcpyHostToDevice);
+
+  // Execute kernel
+  dim3 block(threads);
+  dim3 grid((threads + block.x - 1) / block.x);
+  sumArraysOnDevice<<<grid, block>>>(d_a, d_b, d_c, threads);
+  cudaDeviceSynchronize();
+
+  // Copy data from device to host
+  cudaMemcpy(h_c, d_c, nbytes, cudaMemcpyDeviceToHost);
+
   return 0;
 }
