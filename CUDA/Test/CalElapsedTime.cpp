ab961f26a0846202e88c75f7bf505d759b9cb5ea
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-04-30 12:49:02 +0900

    new file:   CUDA/Test/CalElapsedTime.cpp

---
+++ CUDA/Test/CalElapsedTime.cpp
@@ -0,0 +1,12 @@
+#include "../Hpp/CalElapsedTime.hpp"
+#include <iostream>
+#include <thread>
+
+void doit() {
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+}
+
+auto main() -> decltype(0) {
+  std::cout << CalElapsedTime<>::Execution(doit) << '\n';
+  return 0;
+}
