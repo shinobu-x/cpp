e9d2f77b8a8ade33ecb20526f1fdcae3ecfbb5d8
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-04-30 19:13:01 +0900

    new file:   CUDA/HPP/sumArraysOnDevice.hpp

---
+++ CUDA/HPP/sumArraysOnDevice.hpp
@@ -0,0 +1,9 @@
+#include <cuda_runtime.h>
+
+__global__
+void sumArraysOnDevice(float* a, float* b, float* c, const int thread_max) {
+  int thread = blockDim.x * blockIdx.x + threadIdx.x;
+  if (thread < thread_max) {
+    c[thread] = a[thread] + b[thread];
+  }
+}
