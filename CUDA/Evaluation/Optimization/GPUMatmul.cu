c02777ba559629f14f953b9d9756e88bf238d891
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-05-03 18:05:44 +0900

    modified:   CUDA/Evaluation/Optimization/GPUMatmul.cu

--- CUDA/Evaluation/Optimization/GPUMatmul.cu
+++ CUDA/Evaluation/Optimization/GPUMatmul.cu
@@ -45,21 +45,59 @@ void Init() {
 }
 
 __global__
-void Matmul(value_type* a, value_type* b, value_type* c, int n) {
+void MatmulBad(value_type* a, value_type* b, value_type* c, int n) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   int j = blockIdx.y * blockDim.y + threadIdx.y;
 
   for (int k = 0; k < n; ++k) {
-    c[i * n + j] += a[i * n + k] * b[k * n + j];
+    c[j + i * n] += a[k + i * n] * b[j + k * n];
+  }
+}
+
+__global__
+void MatmulGood(value_type* a, value_type* b, value_type* c, int n) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+  for (int k = 0; k < n; ++k) {
+    c[i + j * n] += a[i + k * n] * b[k + j * n];
   } 
 }
 
-void SpawnKernel() {
-  Matmul<<<N / 256, 256>>>(d_a, d_b, d_c, N);
+__global__
+void MatmulRegister(value_type* a, value_type* b, value_type* c, int n) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  value_type tmp = 0.0f; // Register
+
+  for (int k = 0; k < n; ++k) {
+    tmp += a[k + i * n] * b[j + k * n];
+  }
+
+  c[i + j * n] = tmp;
+}
+
+void SpawnGoodKernel() {
+  MatmulGood<<<N / 128, 128>>>(d_a, d_b, d_c, N);
+  cudaDeviceSynchronize();
+}
+
+void SpawnBadKernel() {
+  MatmulBad<<<N / 128, 128>>>(d_a, d_b, d_c, N);
+  cudaDeviceSynchronize();
+}
+
+void SpawnRegisterKernel() {
+  MatmulRegister<<<N / 128, 128>>>(d_a, d_b, d_c, N);
+  cudaDeviceSynchronize();
 }
 
 auto main() -> decltype(0) {
   Init();
+
+  std::cout << CalElapsedTime<>::Execution(SpawnGoodKernel) << "\n";
+  std::cout << CalElapsedTime<>::Execution(SpawnBadKernel) << "\n";
+  std::cout << CalElapsedTime<>::Execution(SpawnRegisterKernel) << "\n";
 
   cudaFree(d_a);
   cudaFree(d_b);
