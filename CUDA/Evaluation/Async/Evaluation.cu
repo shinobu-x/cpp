b0f6d61ee6f29a44d1d0745ea4c93754663c218f
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-05-06 16:53:21 +0900

    modified:   CUDA/Evaluation/Async/Evaluation.cu modified:   CUDA/Evaluation/Async/ToDo.txt deleted:    CUDA/Async/Evaluation.cu deleted:    CUDA/Async/ToDo.txt

--- CUDA/Evaluation/Async/Evaluation.cu
+++ CUDA/Evaluation/Async/Evaluation.cu
@@ -1,39 +1,48 @@
 // nvprof --print-gpu-trace ./a.out
+#define BOOST_THREAD_VERSION 4
 #include <boost/thread/future.hpp>
 #include <boost/thread.hpp>
+#include <cassert>
 #include <cstdlib>
 #include <CUDA/HPP/InitData.hpp>
 #include <CUDA/HPP/sumArraysOnDevice.hpp>
 
 typedef float value_type;
+const int N = 8;
+std::size_t NBytes = N * sizeof(value_type);
+// cudaStream_t stream = nullptr;
 
-void SpawnKernel(int threads, std::size_t nbytes, cudaStream_t stream) {
+void SpawnKernel(cudaStream_t stream = nullptr) {
   value_type* h_a;
   value_type* h_b;
   value_type* h_c;
-  h_a = (value_type*)malloc(nbytes);
-  h_b = (value_type*)malloc(nbytes);
-  h_c = (value_type*)malloc(nbytes);
+  h_a = (value_type*)malloc(NBytes);
+  h_b = (value_type*)malloc(NBytes);
+  h_c = (value_type*)malloc(NBytes);
 
-  InitData(h_a, threads);
-  InitData(h_b, threads);
+  InitData(h_a, N);
+  InitData(h_b, N);
 
   value_type* d_a;
   value_type* d_b;
   value_type* d_c;
-  cudaMalloc((value_type**)&d_a, nbytes);
-  cudaMalloc((value_type**)&d_b, nbytes);
-  cudaMalloc((value_type**)&d_c, nbytes);
+  cudaMalloc((value_type**)&d_a, NBytes);
+  cudaMalloc((value_type**)&d_b, NBytes);
+  cudaMalloc((value_type**)&d_c, NBytes);
 
-  cudaMemcpy(d_a, h_a, nbytes, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, h_b, nbytes, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_c, h_c, nbytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_a, h_a, NBytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, h_b, NBytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_c, h_c, NBytes, cudaMemcpyHostToDevice);
 
-  dim3 Block(threads);
-  dim3 Grid((threads + Block.x - 1) / Block.x);
-  sumArraysOnDevice<<<Grid, Block, 0, stream>>>(
-    d_a, d_b, d_c, threads);
-  cudaMemcpyAsync(h_c, d_c, nbytes, cudaMemcpyDeviceToHost, stream);
+  dim3 Block(N);
+  dim3 Grid((N + Block.x - 1) / Block.x);
+  if (stream) {
+    sumArraysOnDevice<<<Grid, Block, 0, stream>>>(
+      d_a, d_b, d_c, N);
+    cudaMemcpyAsync(h_c, d_c, NBytes, cudaMemcpyDeviceToHost, stream);
+  } else {
+    sumArraysOnDevice<<<Grid, Block>>>(d_a, d_b, d_c, N);
+  }
   cudaDeviceSynchronize();
 
   free(h_a);
@@ -44,20 +53,56 @@ void SpawnKernel(int threads, std::size_t nbytes, cudaStream_t stream) {
   cudaFree(d_c);
 }
 
-void EvaluateStream() {
-  const int N = 8;
+void DoStream() {
   cudaStream_t s[N];
   value_type* Data[N];
-  std::size_t NBytes = N * sizeof(value_type);
 
   for (int i = 0; i < N; ++i) {
     cudaStreamCreate(&s[i]);
     cudaMalloc(&Data[i], NBytes);
-    SpawnKernel(N, NBytes, s[i]);
+    SpawnKernel(s[i]);
   }
 }
 
+void DoFuture() {
+  boost::future<void> f[N];
+
+  for (auto& f_ : f) {
+    boost::packaged_task<void(cudaStream_t)> t(SpawnKernel);
+    f_ = t.get_future();
+    boost::thread(boost::move(t), nullptr).detach();
+  }
+
+  for (auto& f_ : f) {
+    f_.get();
+    assert(f_.is_ready());
+    assert(f_.has_value());
+    assert(!f_.has_exception());
+    assert(f_.get_state() == boost::future_state::ready);
+  }
+}
+
+void DoAsync() {
+  boost::future<void> f[N];
+
+  for (int i = 0; i < N; ++i) {
+    f[i] = boost::async(boost::launch::async, []() mutable {
+      SpawnKernel(nullptr);
+    });
+  }
+
+  for (auto& f_ : f) {
+    f_.get();
+    assert(f_.is_ready());
+    assert(f_.has_value());
+    assert(!f_.has_exception());
+    assert(f_.get_state() == boost::future_state::ready);
+  }
+
+}
 auto main() -> decltype(0) {
-  EvaluateStream();
+//  DoStream();
+//  DoFuture();
+  DoAsync();
   return 0;
 }
