244425ef7db6011490449c757d0d929587fe621a
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-05-25 20:05:35 +0900

    modified:   Evaluations/Blocking.cpp

--- Evaluations/Blocking.cpp
+++ Evaluations/Blocking.cpp
@@ -17,69 +17,85 @@ void InitData(T* a, T* b) {
 }
 
 template <typename T>
-void Loop0(T* a, T* b) {
-  boost::timer::cpu_timer t;
-  for (int i = 0; i < N; ++i) {
-    for (int j = 0; j < N; ++j) {
-      for (int k = 0; k < N; ++k) {
-        a[i][j] = a[i][k] + b[k][j];
-      }
-    }
-  }
-  std::cout << t.format() << "\n";
-}
-
-template <typename T>
-void Loop1(T* a, T* b) {
-  boost::timer::cpu_timer t;
-  for (int i = 0; i < N; ++i) {
-    for (int k = 0; k < N; ++k) {
+struct Loop0 {
+  void operator()() {
+    T a[N][N];
+    T b[N][N];
+    ::InitData(a, b);
+    boost::timer::cpu_timer t;
+    for (int i = 0; i < N; ++i) {
       for (int j = 0; j < N; ++j) {
-        a[i][j] = a[i][k] + b[k][j];
+        for (int k = 0; k < N; ++k) {
+          a[i][j] = a[i][k] + b[k][j];
+        }
       }
     }
+    std::cout << t.format() << "\n";
   }
-  std::cout << t.format() << "\n";
-}
+};
 
 template <typename T>
-void Loop2(T* a, T* b) {
-  boost::timer::cpu_timer t;
-  int BLOCK = 1 << 3;
-  for (int ii = 0; ii < N; ii += BLOCK) {
-    for (int kk = 0; kk < N; kk += BLOCK) {
-      for (int jj = 0; jj < N; jj += BLOCK) {
-        for (int i = ii; i < ii + BLOCK && i < N; ++i) {
-          for (int k = kk; k < kk + BLOCK && k < N; ++k) {
-            for (int j = jj; j < jj + BLOCK && j < N; ++j) {
-              a[i][j] = a[i][k] + b[k][j];
+struct Loop1 {
+  void operator()() {
+    T a[N][N];
+    T b[N][N];
+    ::InitData(a, b);
+    boost::timer::cpu_timer t;
+    for (int i = 0; i < N; ++i) {
+      for (int k = 0; k < N; ++k) {
+        for (int j = 0; j < N; ++j) {
+          a[i][j] = a[i][k] + b[k][j];
+        }
+      }
+    }
+    std::cout << t.format() << "\n";
+  }
+};
+
+template <typename T>
+struct Loop2 {
+  void operator()() {
+    T a[N][N];
+    T b[N][N];
+    ::InitData(a, b);
+    boost::timer::cpu_timer t;
+    int BLOCK = 1 << 6;
+    for (int ii = 0; ii < N; ii += BLOCK) {
+      for (int kk = 0; kk < N; kk += BLOCK) {
+        for (int jj = 0; jj < N; jj += BLOCK) {
+          for (int i = ii; i < ii + BLOCK && i < N; ++i) {
+            for (int k = kk; k < kk + BLOCK && k < N; ++k) {
+              for (int j = jj; j < jj + BLOCK && j < N; ++j) {
+                a[i][j] = a[i][k] + b[k][j];
+              }
             }
           }
         }
       }
     }
+    std::cout << t.format() << "\n";
   }
-  std::cout << t.format() << "\n";
-}
+};
 
 template <typename T>
 void DoIt() {
   typedef T value_type;
-  value_type a[N][N];
-  value_type b[N][N];
 
-  InitData(a, b);
-  Loop0(a, b);
-
-  boost::this_thread::sleep_for(boost::chrono::seconds(3));
-
-  InitData(a, b);
-  Loop1(a, b);
+  Loop0<value_type> L0;
+  boost::thread l0(L0);
+  l0.join();
 
   boost::this_thread::sleep_for(boost::chrono::seconds(3));
 
-  InitData(a, b);
-  Loop1(a, b);
+  Loop1<value_type> L1;
+  boost::thread l1(L1);
+  l1.join();
+
+  boost::this_thread::sleep_for(boost::chrono::seconds(3));
+
+  Loop2<value_type> L2;
+  boost::thread l2(L2);
+  l2.join();
 }
 
 auto main() -> decltype(0) {
