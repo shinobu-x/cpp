84d1df2be81b31265f52cad0552a35ed9ce50d25
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-07-06 19:36:35 +0900

    new file:   Fun/0x0004.cpp

---
+++ Fun/0x0004.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+
+#define INF 10000
+int N = 4;
+int w[INF] = {2, 1, 3, 2};
+int v[INF] = {3, 2, 4, 2};
+int W = 5;
+int dp[INF];
+
+auto main() -> decltype(0) {
+  memset(dp, -1, sizeof(dp));
+  return 0;
+}
