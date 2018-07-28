b4806173b5ca1c9765dbd96c61f06979bffe9a90
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-07-28 18:48:14 +0900

    modified:   test/graph/test_adjacency_list.cpp

--- test/graph/test_adjacency_list.cpp
+++ test/graph/test_adjacency_list.cpp
@@ -1,5 +1,6 @@
 #include <boost/graph/adjacency_list.hpp>
 
+#include <iostream>
 #include <list>
 #include <set>
 #include <vector>
@@ -14,19 +15,6 @@
             class EdgeListS = listS>            # グラフの辺リストのコンテナ
   class adjacency_list;
 */
-
-// 無向グラフ
-typedef boost::adjacency_list<
-  boost::listS,
-  boost::vecS,
-  boost::undirectedS
-  > UndirectedGraph;
-
-typedef boost::graph_traits<UndirectedGraph>::vertex_descriptor UGVDescriptor;
-typedef boost::graph_traits<UndirectedGraph>::edge_descriptor UGEDescriptor;
-typedef boost::graph_traits<UndirectedGraph>::vertex_iterator UGVIterator;
-typedef boost::graph_traits<UndirectedGraph>::edge_iterator UGEIterator;
-
 // 有向グラフ
 typedef boost::adjacency_list<
   boost::listS,
@@ -42,11 +30,27 @@ typedef boost::graph_traits<DirectedGraph>::edge_descriptor DGEDescriptor;
 typedef boost::graph_traits<DirectedGraph>::vertex_iterator DGVIterator;
 typedef boost::graph_traits<DirectedGraph>::edge_descriptor DGEIterator;
 
+// 無向グラフ
+typedef boost::adjacency_list<
+  boost::listS,
+  boost::vecS,
+  boost::undirectedS,
+  boost::no_property,
+  boost::property<
+    boost::edge_weight_t, int>
+  > UndirectedGraph;
+
+typedef boost::graph_traits<UndirectedGraph>::vertex_descriptor UGVDescriptor;
+typedef boost::graph_traits<UndirectedGraph>::edge_descriptor UGEDescriptor;
+typedef boost::graph_traits<UndirectedGraph>::vertex_iterator UGVIterator;
+typedef boost::graph_traits<UndirectedGraph>::edge_iterator UGEIterator;
+
 // 双方向グラフ
 typedef boost::adjacency_list<
   boost::listS,
   boost::vecS,
   boost::bidirectionalS,
+  boost::no_property,
   boost::property<
     boost::edge_weight_t, int>
   > BidirectedGraph;
@@ -57,19 +61,39 @@ typedef boost::graph_traits<BidirectedGraph>::vertex_iterator BGVIterator;
 typedef boost::graph_traits<BidirectedGraph>::edge_iterator BGEIterator;
 
 auto main() -> decltype(0) {
-  UndirectedGraph ug_u, ug_v;
   DirectedGraph dg_u, dg_v;
+  UndirectedGraph ug_u, ug_v;
   BidirectedGraph bg_u, bg_v;
 
+  typedef boost::property<boost::edge_name_t, int> edge_property;
   const int vertices = 9;
   int capacity[] = {
     10, 20, 20, 20, 40, 40, 20, 20, 20, 10};
   int flow[] = {
     8, 12, 12, 12, 12, 12, 16, 16, 16, 8};
 
-  /**
-   * include/boost/graph/detail/adjacency_list.hpp
-   * add_edge(...)
-   */
+/**
+ // include/boost/graph/detail/adjacency_list.hpp
+ Method: add_edge
+
+ O(1) for allow_parallel_edge_tag
+ O(log(E/V)) for disallow_parallel_edge_tag
+
+ Directed | Undirected | Bidirected Graphs
+
+ Input: vertex_descriptor,
+        vertex_descriptor,
+        edge_property_type&,
+        directed_graph_helper& |
+        undirected_graph_helper& |
+        bidirectional_graph_helper_with_property&
+
+ Return Type: std::pair
+        Value: edge_descriptor, bool
+
+*/
+  boost::add_edge(1, 2, 1, dg_u);
+  boost::add_edge(1, 2, 1, ug_u);
+  boost::add_edge(1, 2, 1, bg_u);
   return 0;
 }
