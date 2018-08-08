beccf8448c989322a9f22ab6900695a1bb3a5156
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-08-08 19:12:02 +0900

    modified:   test/graph/test_adjacency_list.cpp

--- test/graph/test_adjacency_list.cpp
+++ test/graph/test_adjacency_list.cpp
@@ -6,11 +6,35 @@
 #include <vector>
 
 /**
-  // The Graph concepts
+  # コンセプト
   template <class G>
   struct GraphConcept {
+    * vertex_descriptor      # 頂点の表現方法
+    * edge_descriptor        # 辺の表現方法
+    * directed_category      # 有向／無向の指定
+    * edge_paralell_category # 有向グラフのエッジ追加の際の逆向きエッジの指定
+    * traversal_category     # 頂点、エッジへのアクセス方法を指定
+    *   # アクセス方法
+    *   # 接続グラフ
+    *   # 任意の頂点から出るエッジに対するイテレーション
+    *   boost::incidence_graph_tag
+    *   # 隣接グラフ
+    *   # 任意の頂点から出るエッジの先の頂点に対するイテレーション
+    *   boost::adjacency_graph_tag
+    *   # 双方向グラフ
+    *   # 任意の頂点から入るエッジに対するイテレーション
+    *   boost::bidirectional_graph_tag
+    *   # 頂点リスト
+    *   # 全ての頂点に対するイテレーション
+    *   boost::vertex_list_graph_tag
+    *   # 辺リスト
+    *   # 全ての辺に対するイテレーション
+    *   boost::edge_list_graph_tag
+    *   # 隣接行列
+    *   # 2頂点間のエッジの有無の判定
+    *   boost::adjacency_matrix_tag
     typedef typename boost::graph_traits<G>::vertex_descriptor
-      vertexdescriptor;
+      vertex_descriptor;
     typedef typename boost::graph_traits<G>::edge_descriptor
       edge_descriptor;
     typedef typename boost::graph_traits<G>::directed_category
@@ -20,7 +44,7 @@
     typedef typename boost::graph_traits<G>::traversal_category
       traversal_category;
 
-    // Boost concept checks
+    # Boost concept checks
     void constraints() {
       BOOST_CONCEPT_ASSERT((DefaultConstructibleConcept<vertex_descriptor>));
       BOOST_CONCEPT_ASSERT((EqualityComparableConcept<vertex_descriptor>));
@@ -38,7 +62,7 @@
             class EdgeProperty = no_property,   # 辺のカスタムプロパティ
             class GraphProperty = no_property,  # グラフのカスタムプロパティ
             class EdgeListS = listS>            # グラフの辺リストのコンテナ
-  class adjacency_list; 
+  class adjacency_list;
 */
 struct VertexProperty {
   int Id;
