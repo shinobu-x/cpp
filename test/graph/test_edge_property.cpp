114380e96aad9aac007cf304417057e799da13a9
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-08-16 19:35:41 +0900

    modified:   test/graph/test_edge_property.cpp

--- test/graph/test_edge_property.cpp
+++ test/graph/test_edge_property.cpp
@@ -33,26 +33,26 @@ struct Graph {
     boost::get(edge_xflow, G);
 
   void operator()(T& g) {
-    vertex_iterator it1, end1;
-    boost::tie(it1, end1) = boost::vertices(G);
 
-    for (; it1 != end1; ++it1) {
+    std::pair<vertex_iterator, vertex_iterator> v = boost::vertices(G);
+    for (; v.first != v.second; ++v.first) {
 
-      out_edge_iterator it2, end2;
       // Capacity and flow from source to destination edges
-      boost::tie(it2, end2) = boost::out_edges(*it1, G);
-      for (; it2 != end2; ++it2) {
-        std::cout << "-- [Capacity] " << capacity[*it2] << ", "
-          << " [Flow] " << flow[*it2] << " --> "
-          << boost::target(*it2, G) << "\t";
+      boost::graph_traits<boost::adjacency_list<T> > oe =
+        boost::out_edges(*v.first, G);
+      for (; oe.first != oe.second; ++oe.fisrt) {
+        std::cout << "-- [Capacity] " << capacity[*oe.first] << ", "
+          << " [Flow] " << flow[*oe.first] << " --> "
+          << boost::target(*oe.first, G) << "\t";
       }
 
-      in_edge_iterator it3, end3;
       // Capacity and flow from destination to source edges
-      boost::tie(it3, end3) = boost::in_edges(*it1, G);
-      for (; it3 != end3; ++it3) {
-        std::cout << " <-- " << capacity[*it3] << " [Capacity], "
-          << flow[*it3] << " [Flow] " << boost::source(*it3, G) << "\n";
+      std::pair<in_edge_iterator, in_edge_iterator> ie =
+        boost::in_edges(*v.first, G);
+      for (; ie.first != ie.second; ++ie.first) {
+        std::cout << " <-- " << capacity[*ie.first] << " [Capacity], "
+          << flow[*ie.first] << " [Flow] " << boost::source(*ie.first, G)
+          << "\n";
       }
     }
 
@@ -63,6 +63,38 @@ struct Graph {
 };
 
 auto main() -> decltype(0) {
+  typedef boost::property<edge_xcapacity_t, int> capacity;
+  typedef boost::property<edge_xflow_t, int, capacity> flow;
+  typedef boost::adjacency_list<boost::vecS,
+                                boost::vecS,
+                                boost::bidirectionalS,
+                                boost::no_property,
+                                flow> G;
+
+  const int num_vertices = 9;
+  G g(num_vertices);
+
+  /**
+   *          e10 <--- e9 <--- e8
+   *           |               ^
+   *           v               |
+   *   e0 ---> e1 ---> e5 ---> e7 ---> e11
+   *           |               ^
+   *           v               |
+   *           e2 ---> e4 ---> e6
+   */
+  boost::add_edge(0, 1, flow(10, capacity(8)), g);
+  boost::add_edge(1, 2, flow(3, capacity(9)), g);
+  boost::add_edge(2, 4, flow(5, capacity(10)), g);
+  boost::add_edge(4, 6, flow(8, capacity(5)), g);
+  boost::add_edge(6, 7, flow(8, capacity(8)), g);
+  boost::add_edge(7, 8, flow(10, capacity(12)), g);
+  boost::add_edge(8, 9, flow(5, capacity(7)), g);
+  boost::add_edge(9, 10, flow(7, capacity(11)), g);
+  boost::add_edge(10, 1, flow(9, capacity(6)), g);
+  boost::add_edge(1, 5, flow(8, capacity(15)), g);
+  boost::add_edge(5, 7, flow(5, capacity(9)), g);
+  boost::add_edge(7, 11, flow(10, capacity(9)), g);
 
   return 0;
 }
