38c9cc55920b6845be32226005c2579080e2f11e
Shinobu Kinjo <shinobu.kj@gmail.com>
2018-08-23 19:37:03 +0900

    modified:   test/graph/test_vertex.cpp

--- test/graph/test_vertex.cpp
+++ test/graph/test_vertex.cpp
@@ -1,5 +1,6 @@
 #include <boost/graph/adjacency_list.hpp>
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -7,12 +8,7 @@ enum vertex_location_t {
   vertex_location
 };
 
-
-// namespace boost {
-//   BOOST_INSTALL_PROPERTY(vertex, location);
-// }
-
-// BOOST_INSTALL_PROPERTY
+// include/boost/graph/properties.hpp
 namespace boost {
   template <>
   struct property_kind<vertex_location_t> {
@@ -33,20 +29,32 @@ auto main() -> decltype(0) {
 
   typedef typename boost::property_map<G, vertex_location_t>::type location_m;
   typedef typename boost::property_traits<location_m>::value_type location_t;
+  typedef std::pair<int, int> vertices;
 
   G g;
+
   // boost::property<vertex_location_t,
   //                 std::vector<std::string>
   //                >
   location_p l;
 
+  std::pair<int, int> edges[10] = {vertices(0, 1)};
+  boost::add_edge(edges[0].first, edges[0].second, g);
+
   boost::property_map<G, vertex_location_t>::type loc_t =
     boost::get(vertex_location_t(), g);
 
-  std::vector<std::string> v;
-  v.push_back("A");
+  std::vector<std::string> v1;
+  std::vector<std::string> v2;
 
-  boost::put(loc_t, 0, v);
+  v1.push_back("a");
+  v1.push_back("A");
+
+  v2.push_back("b");
+  v2.push_back("B");
+
+  boost::put(loc_t, 0, v1);
+  boost::put(loc_t, 1, v2);
 
   // boost::property_map<G,
   //                     vertex_location_t
@@ -60,6 +68,12 @@ auto main() -> decltype(0) {
   //                       >::value_type
   location_t s = boost::get(loc_m, boost::source(*es.first, g));
   location_t t = boost::get(loc_m, boost::target(*es.first, g));
+
+  std::cout << s[0] << "\n";
+  std::cout << t[0] << "\n";
+
+  std::cout << s[1] << "\n";
+  std::cout << t[1] << "\n";
 
   return 0;
 }
